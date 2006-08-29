6749464b6ac7405c81e9e6c3d94b914c4d895d26
rstarkov <sourcecode@roman.st>
2006-08-29 16:50:49 +0100

    Collection of generic GUI controls as a separate project. Main branch. (imported from Perforce changelist 34)

---
+++ Src/Controls/Separator.Designer.cs
@@ -0,0 +1,60 @@
+namespace RT.Controls
+{
+    partial class Separator
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GB = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // GB
+            // 
+            this.GB.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.GB.Location = new System.Drawing.Point(-8, 1);
+            this.GB.Name = "GB";
+            this.GB.Size = new System.Drawing.Size(144, 34);
+            this.GB.TabIndex = 6;
+            this.GB.TabStop = false;
+            this.GB.Text = "Separator";
+            // 
+            // Separator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.GB);
+            this.Name = "Separator";
+            this.Size = new System.Drawing.Size(120, 16);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox GB;
+    }
+}
---
+++ Src/Controls/Separator.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RT.Controls
+{
+    public partial class Separator : UserControl
+    {
+        public Separator()
+        {
+            InitializeComponent();
+        }
+
+        [Browsable(true)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+        public override string Text
+        {
+            get { return GB.Text; }
+            set { GB.Text = value; }
+        }
+    }
+}
