7a9a4d7c3e159950d90cae15ca718956028c2893
rstarkov <sourcecode@roman.st>
2006-10-24 23:46:17 +0100

    Various improvements to the name tool; ExpertVersion logo; splitcontainerex (imported from Perforce changelist 148)

--- Src/Controls/SplitContainerEx.cs
+++ Src/Controls/SplitContainerEx.cs
@@ -8,6 +8,9 @@ using System.Windows.Forms;
 
 namespace RT.Controls
 {
+    /// <summary>
+    /// SplitContainerEx provides such "advanced" features as painting the bloody splitter.
+    /// </summary>
     public partial class SplitContainerEx : SplitContainer
     {
         public SplitContainerEx()
@@ -39,20 +42,18 @@ namespace RT.Controls
             if (Orientation == Orientation.Horizontal)
             {
                 // Horizontal
-                e.Graphics.DrawLine(Pens.White, 0, mid, ClientRectangle.Width, mid);
-                e.Graphics.DrawLine(Pens.Gray, 0, mid+1, ClientRectangle.Width, mid+1);
-                //mid = (Panel2.Top + Panel1.Bottom) / 2 + 2;
-                //e.Graphics.DrawLine(Pens.White, 0, mid, ClientRectangle.Width, mid);
-                //e.Graphics.DrawLine(Pens.Gray, 0, mid + 1, ClientRectangle.Width, mid + 1);
+                e.Graphics.DrawLine(Pens.White, 0, mid-2, ClientRectangle.Width, mid-2);
+                e.Graphics.DrawLine(Pens.Gray,  0, mid-1, ClientRectangle.Width, mid-1);
+                e.Graphics.DrawLine(Pens.White, 0, mid,   ClientRectangle.Width, mid);
+                e.Graphics.DrawLine(Pens.Gray,  0, mid+1, ClientRectangle.Width, mid+1);
             }
             else
             {
                 // Vertical
-                e.Graphics.DrawLine(Pens.White, mid, 0, mid, ClientRectangle.Height);
-                e.Graphics.DrawLine(Pens.Gray, mid+1, 0, mid+1, ClientRectangle.Height);
-                //mid = (Panel2.Left + Panel1.Right) / 2 + 2;
-                //e.Graphics.DrawLine(Pens.White, mid, 0, mid, ClientRectangle.Height);
-                //e.Graphics.DrawLine(Pens.Gray, mid + 1, 0, mid + 1, ClientRectangle.Height);
+                e.Graphics.DrawLine(Pens.White, mid-2, 0, mid-2, ClientRectangle.Height);
+                e.Graphics.DrawLine(Pens.Gray,  mid-1, 0, mid-1, ClientRectangle.Height);
+                e.Graphics.DrawLine(Pens.White, mid,   0, mid,   ClientRectangle.Height);
+                e.Graphics.DrawLine(Pens.Gray,  mid+1, 0, mid+1, ClientRectangle.Height);
             }
         }
     }
