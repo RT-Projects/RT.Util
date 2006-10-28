a9da2ac2cd28361c93476108e2a67c240e992672
rstarkov <sourcecode@roman.st>
2006-10-28 12:55:07 +0100

    Merged Controls into Util (imported from Perforce changelist 152)

--- Src/Resources/Resources.Designer.cs
+++ Src/Resources/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace RT.Controls.Properties {
+namespace RT.Util.Resources {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace RT.Controls.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("RT.Controls.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("RT.Util.Resources.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
