73a1de74ca31900f5b8ddc509987158fb3c9f3bd
rstarkov <sourcecode@roman.st>
2006-12-05 00:07:08 +0000

    Added ConfirmEvent and InternalException to Utils. (imported from Perforce changelist 229)

--- Src/Util.cs
+++ Src/Util.cs
@@ -7,9 +7,30 @@ using System.Drawing;
 using System.Threading;
 using System.Windows.Forms;
 using System.Diagnostics;
+using RT.Util.Dialogs;
 
 namespace RT.Util
 {
+    #region ConfirmEvent
+
+    /// <summary>
+    /// EventArgs for the ConfirmEventHandler delegate.
+    /// </summary>
+    public class ConfirmEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Set this to true/false to confirm or cancel the action.ssss
+        /// </summary>
+        public bool ConfirmOK;
+    }
+
+    /// <summary>
+    /// A general-purpose event type which lets the caller confirm an action.
+    /// </summary>
+    public delegate void ConfirmEventHandler(object sender, ConfirmEventArgs e);
+
+    #endregion
+
     /// <summary>
     /// This class offers some generic static functions which are hard to categorize
     /// under any more specific classes.
@@ -86,5 +107,61 @@ namespace RT.Util
                 return (size / (1024d * 1024d * 1024d)).ToString("#,###.## G");
             }
         }
+
+        #region Internal error
+
+        /// <summary>
+        /// Informs the user nicely that an internal error has occurred. Does not
+        /// actually terminate the program, so if the problem is not fatal the user
+        /// could save their work etc.
+        /// </summary>
+        public static void InternalError()
+        {
+            InternalError(null);
+        }
+
+        /// <summary>
+        /// Informs the user nicely that an internal error has occurred. Does not
+        /// actually terminate the program, so if the problem is not fatal the user
+        /// could save their work etc.
+        /// </summary>
+        /// <param name="ErrorCode">Some error code to help the developer pinpoint the problem.</param>
+        public static void InternalError(int ErrorCode)
+        {
+            InternalErrorPrivate(ErrorCode.ToString());
+        }
+
+        /// <summary>
+        /// Informs the user nicely that an internal error has occurred. Does not
+        /// actually terminate the program, so if the problem is not fatal the user
+        /// could save their work etc.
+        /// </summary>
+        /// <param name="ErrorCode">Some error message to help the developer pinpoint the problem.</param>
+        public static void InternalError(string ErrorMsg)
+        {
+            InternalErrorPrivate(ErrorMsg);
+        }
+
+        /// <summary>
+        /// Raises an internal error. The reason why this is private is so that only
+        /// the other InternalError functions can call it. This ensures that the
+        /// function can always backtrack exactly 2 stack frames to get to the place
+        /// which invoked InternalError.
+        /// </summary>
+        private static void InternalErrorPrivate(string ErrorMsg)
+        {
+            string str = "Internal application error has occurred. Please inform the developer.\n";
+            if (ErrorMsg != null) str += "\nError message: " + ErrorMsg;
+            StackFrame sf = new StackFrame(2, true);
+            str += "\nMethod: " + sf.GetMethod();
+            if (sf.GetFileLineNumber() != 0)
+                // The line number is zero if there is no program database in the
+                // application directory (which is the case when a normal user runs it)
+                str += "\nFile: " + sf.GetFileName() + "\nLine: " + sf.GetFileLineNumber();
+
+            DlgMessage.ShowError(str, "Internal error", "OK");
+        }
+
+        #endregion
     }
 }
