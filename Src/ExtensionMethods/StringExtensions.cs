c377a6e623cec2802226db20dd53a4f849157f2b
Timwi <github@timwi.de>
2008-09-21 14:42:55 +0100

    Move string extension methods and DynamicContentStream from Servers to Util (imported from Perforce changelist 533)

---
+++ Src/ExtensionMethods/StringExtensions.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+
+namespace RT.Util.ExtensionMethods
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Escapes all necessary characters in the specified string so as to make it usable safely in an HTML or XML context.
+        /// </summary>
+        /// <param name="Input">The string to apply HTML or XML escaping to.</param>
+        /// <returns>The specified string with the necessary HTML or XML escaping applied.</returns>
+        public static string HTMLEscape(this string Input)
+        {
+            return Input.Replace("&", "&amp;").Replace("<", "&lt;").Replace(">", "&gt;").Replace("'", "&#39;").Replace("\"", "&quot;");
+        }
+
+        /// <summary>
+        /// Escapes all necessary characters in the specified string so as to make it usable safely in a URL.
+        /// </summary>
+        /// <param name="Input">The string to apply URL escaping to.</param>
+        /// <returns>The specified string with the necessary URL escaping applied.</returns>
+        public static string URLEscape(this string Input)
+        {
+            byte[] UTF8 = Input.ToUTF8();
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in UTF8)
+                sb.Append((b >= 'a' && b <= 'z') || (b >= 'A' && b <= 'Z') || (b >= '0' && b <= '9')
+                    || (b == '$') || (b == '-') || (b == '_') || (b == '.') || (b == '!') || (b == '/')
+                    || (b == '*') || (b == '\'') || (b == '(') || (b == ')') || (b == ',')
+                    ? ((char) b).ToString() : string.Format("%{0:X2}", b));
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Reverses the escaping performed by <see cref="URLEscape"/>() by decoding hexadecimal URL escape sequences into their original characters.
+        /// </summary>
+        /// <param name="Input">String containing URL escape sequences to be decoded.</param>
+        /// <returns>The specified string with all URL escape sequences decoded.</returns>
+        public static string URLUnescape(this string Input)
+        {
+            if (Input.Length < 3)
+                return Input;
+            int BufferSize = 0;
+            int i = 0;
+            while (i < Input.Length)
+            {
+                BufferSize++;
+                if (Input[i] == '%') { i += 2; }
+                i++;
+            }
+            byte[] Buffer = new byte[BufferSize];
+            BufferSize = 0;
+            i = 0;
+            while (i < Input.Length)
+            {
+                if (Input[i] == '%' && i < Input.Length - 2)
+                {
+                    try
+                    {
+                        Buffer[BufferSize] = byte.Parse("" + Input[i + 1] + Input[i + 2], NumberStyles.HexNumber);
+                        BufferSize++;
+                    }
+                    catch (Exception) { }
+                    i += 3;
+                }
+                else
+                {
+                    Buffer[BufferSize] = Input[i] == '+' ? (byte) ' ' : (byte) Input[i];
+                    BufferSize++;
+                    i++;
+                }
+            }
+            return Encoding.UTF8.GetString(Buffer, 0, BufferSize);
+        }
+
+        /// <summary>
+        /// Converts the specified string to UTF-8.
+        /// </summary>
+        /// <param name="Input">String to convert to UTF-8.</param>
+        /// <returns>The specified string, converted to a byte-array containing the UTF-8 encoding of the string.</returns>
+        public static byte[] ToUTF8(this string Input)
+        {
+            return Encoding.UTF8.GetBytes(Input);
+        }
+
+        /// <summary>
+        /// Determines the length of the UTF-8 encoding of the specified string.
+        /// </summary>
+        /// <param name="Input">String to determined UTF-8 length of.</param>
+        /// <returns>The length of the string in bytes when encoded as UTF-8.</returns>
+        public static int UTF8Length(this string Input)
+        {
+            return Encoding.UTF8.GetByteCount(Input);
+        }
+
+        /// <summary>
+        /// Converts the specified string to a byte array. Non-ASCII characters are replaced with question marks ('?').
+        /// </summary>
+        /// <param name="Input">String to convert to a byte array.</param>
+        /// <returns>The specified string, converted to a byte-array with non-ASCII characters replaced with question marks ('?').</returns>
+        public static byte[] ToASCII(this string Input)
+        {
+            return Encoding.ASCII.GetBytes(Input);
+        }
+    }
+}
---
+++ Src/Streams/DynamicContentStream.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using RT.Util.ExtensionMethods;
+
+namespace RT.Util.Streams
+{
+    /// <summary>
+    /// Provides a read-only stream that can "read from" an <see cref="IEnumerable&lt;string&gt;"/>.
+    /// In particular, an intended application is to "read from" a method that uses "yield return" to return strings as execution proceeds.
+    /// This enables generation of, for example, HTML for dynamic web pages.
+    /// </summary>
+    public class DynamicContentStream : Stream
+    {
+        private IEnumerator<string> Enumerator = null;
+        private byte[] LastUnprocessedBytes = null;
+        private int LastUnprocessedBytesIndex = 0;
+
+        /// <summary>
+        /// Instantiates a buffered <see cref="DynamicContentStream"/>.
+        /// </summary>
+        /// <param name="Enumerable">The object that provides the content for this stream to read from.</param>
+        public DynamicContentStream(IEnumerable<string> Enumerable)
+        {
+            this.Enumerator = Enumerable.GetEnumerator();
+        }
+
+        /// <summary>
+        /// Instantiates a <see cref="DynamicContentStream"/> and lets you configure whether it's buffered or not.
+        /// </summary>
+        /// <param name="Enumerable">The object that provides the content for this stream to read from.</param>
+        /// <param name="Buffered">Provides an initial value for the <see cref="Buffered"/> property.</param>
+        public DynamicContentStream(IEnumerable<string> Enumerable, bool Buffered)
+        {
+            this.Enumerator = Enumerable.GetEnumerator();
+            this.Buffered = Buffered;
+        }
+
+        public override bool CanRead { get { return true; } }
+        public override bool CanSeek { get { return false; } }
+        public override bool CanWrite { get { return false; } }
+        public override void Flush() { }
+
+        /// <summary>
+        /// If true, each call to <see cref="Read()"/> will move the enumerator forward as far as necessary to fill the buffer.
+        /// If false, each call to <see cref="Read()"/> returns only the text produced by a single MoveNext() of the enumerator.
+        /// </summary>
+        public bool Buffered { get; set; }
+
+        /// <summary>
+        /// Reads some text into the specified buffer. The behaviour depends on the <see cref="Buffered"/> property.
+        /// The bytes returned respresent the text returned by the underlying enumerator, UTF-8-encoded.
+        /// Although DynamicContentStream makes no effort to keep multi-byte characters within a single invocation
+        /// of Read(), all output will be valid UTF-8 when concatenated.
+        /// </summary>
+        /// <param name="buffer">The buffer to copy the data into.</param>
+        /// <param name="offset">The offset at which to start copying into buffer.</param>
+        /// <param name="count">The maximum number of bytes to copy. The stream may return less than this.</param>
+        /// <returns>The number of bytes actually copied into the buffer.</returns>
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            if (LastUnprocessedBytes != null && LastUnprocessedBytes.Length > 0)
+            {
+                if (LastUnprocessedBytes.Length - LastUnprocessedBytesIndex > count)
+                {
+                    Array.Copy(LastUnprocessedBytes, LastUnprocessedBytesIndex, buffer, offset, count);
+                    LastUnprocessedBytesIndex += count;
+                    return count;
+                }
+                else
+                {
+                    int HowMany = LastUnprocessedBytes.Length - LastUnprocessedBytesIndex;
+                    Array.Copy(LastUnprocessedBytes, LastUnprocessedBytesIndex, buffer, offset, HowMany);
+                    LastUnprocessedBytes = null;
+                    LastUnprocessedBytesIndex = 0;
+                    return HowMany;
+                }
+            }
+
+            if (Buffered)
+            {
+                StringBuilder b = new StringBuilder();
+                long BytesSoFar = 0;
+                while (BytesSoFar < count)
+                {
+                    if (!Enumerator.MoveNext())
+                        break;
+                    b.Append(Enumerator.Current);
+                    BytesSoFar += Enumerator.Current.UTF8Length();
+                }
+                if (b.Length == 0)
+                    return 0;
+
+                byte[] BigBuffer = b.ToString().ToUTF8();
+                if (BigBuffer.Length > count)
+                {
+                    Array.Copy(BigBuffer, 0, buffer, offset, count);
+                    LastUnprocessedBytes = BigBuffer;
+                    LastUnprocessedBytesIndex = count;
+                    return count;
+                }
+                else
+                {
+                    Array.Copy(BigBuffer, 0, buffer, offset, BigBuffer.Length);
+                    LastUnprocessedBytes = null;
+                    return BigBuffer.Length;
+                }
+            }
+            else
+            {
+                do
+                {
+                    if (!Enumerator.MoveNext())
+                        return 0;
+                } while (Enumerator.Current.Length == 0);
+                byte[] Encoded = Enumerator.Current.ToUTF8();
+                if (Encoded.Length > count)
+                {
+                    Array.Copy(Encoded, 0, buffer, 0, count);
+                    LastUnprocessedBytes = Encoded;
+                    LastUnprocessedBytesIndex = count;
+                    return count;
+                }
+                else
+                {
+                    Array.Copy(Encoded, 0, buffer, 0, Encoded.Length);
+                    return Encoded.Length;
+                }
+            }
+        }
+
+        // Things you can't do
+        public override long Length { get { throw new NotSupportedException(); } }
+        public override long Seek(long offset, SeekOrigin origin) { throw new NotSupportedException(); }
+        public override void SetLength(long value) { throw new NotSupportedException(); }
+        public override void Write(byte[] buffer, int offset, int count) { throw new NotSupportedException(); }
+        public override long Position
+        {
+            get
+            {
+                throw new NotSupportedException();
+            }
+            set
+            {
+                throw new NotSupportedException();
+            }
+        }
+    }
+}
