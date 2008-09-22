3f1ca7bc3361c8ecb4b1d2bc6f579cf29136a03a
Timwi <github@timwi.de>
2008-09-22 12:48:35 +0100

    Extension method for Dictionary<TKey, List<TValue>> (imported from Perforce changelist 538)

---
+++ Src/ExtensionMethods/DictionaryExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RT.Util.ExtensionMethods
+{
+    public static class DictionaryExtensions
+    {
+        public static void AddSafe<K, V>(this Dictionary<K, List<V>> Dic, K Key, V Value)
+        {
+            if (!Dic.ContainsKey(Key))
+                Dic[Key] = new List<V>();
+            Dic[Key].Add(Value);
+        }
+    }
+}
