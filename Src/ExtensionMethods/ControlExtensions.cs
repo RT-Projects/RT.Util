2144df3897805e1bce3520481face25dc71f0502
rstarkov <sourcecode@roman.st>
2008-12-15 07:53:31 +0000

    ControlExtensions added. (imported from Perforce changelist 699)

---
+++ Src/ExtensionMethods/ControlExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RT.Util.ExtensionMethods
+{
+    /// <summary>
+    /// Extension methods for Windows Forms controls.
+    /// </summary>
+    public static class ControlExtensions
+    {
+        /// <summary>
+        /// If this control is located within a <see cref="TabPage"/>, returns that TabPage
+        /// by iterating recursively through this item's parents. Otherwise returns null.
+        /// </summary>
+        public static TabPage ParentTab(this Control control)
+        {
+            while (control != null)
+            {
+                if (control.Parent is TabPage)
+                    return control.Parent as TabPage;
+                control = control.Parent;
+            }
+            return null;
+        }
+    }
+}
