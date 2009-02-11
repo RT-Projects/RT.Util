3ba31d45c3f36e15227ce2ce7e4d677566a311c9
Timwi <github@timwi.de>
2009-02-11 19:43:38 +0000

    Add IntegerExtensions (imported from Perforce changelist 759)

---
+++ Src/ExtensionMethods/IntegerExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RT.Util.ExtensionMethods
+{
+    /// <summary>
+    /// Provides extension methods on built-in integer types such as <see cref="Int32"/> etc.
+    /// </summary>
+    public static class IntegerExtensions
+    {
+        /// <summary>
+        /// Bitwise rotate left.
+        /// </summary>
+        /// <param name="op">How many bits to rotate by.</param>
+        public static ulong RotateLeft(this ulong input, int op)
+        {
+            return (input << op) | (input >> (64 - op));
+        }
+    }
+}
