c5ac009496663cb0848ded1ea3d64388c4c74cae
Timwi <github@timwi.de>
2009-03-06 16:45:30 +0000

    Run all tests using Reflection (imported from Perforce changelist 775)

--- Src/UtilTests.cs
+++ Src/UtilTests.cs
@@ -3,18 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using RT.Util.ExtensionMethods;
+using System.Reflection;
+using NUnit.Framework;
 
 namespace RT.Util
 {
     static class UtilTests
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            // Note: this is not supposed to list every single test, since keeping the list
-            // up-to-date would be a chore. Instead this is here so that individual tests
-            // can be executed and debugged using Visual Studio rather than NUnit.
+            foreach (var ty in Assembly.GetExecutingAssembly().GetExportedTypes().Where(t => t.GetCustomAttributes(typeof(TestFixtureAttribute), true).Any()))
+            {
+                Console.WriteLine("Testing type: " + ty);
+                var sts = Activator.CreateInstance(ty);
 
-            Console.WriteLine("Done testing.");
+                foreach (var meth in ty.GetMethods().Where(m => m.GetCustomAttributes(typeof(TestFixtureSetUpAttribute), false).Any()))
+                {
+                    Console.WriteLine("-- Running setup: " + meth.Name);
+                    meth.Invoke(sts, new object[] { });
+                }
+
+                foreach (var meth in ty.GetMethods().Where(m => m.GetCustomAttributes(typeof(TestAttribute), false).Any()))
+                {
+                    Console.WriteLine("-- Running test: " + meth.Name);
+                    meth.Invoke(sts, new object[] { });
+                }
+
+                foreach (var meth in ty.GetMethods().Where(m => m.GetCustomAttributes(typeof(TestFixtureTearDownAttribute), false).Any()))
+                {
+                    Console.WriteLine("-- Running teardown: " + meth.Name);
+                    meth.Invoke(sts, new object[] { });
+                }
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Tests passed; press Enter to exit.");
+            Console.ReadLine();
         }
     }
 }
