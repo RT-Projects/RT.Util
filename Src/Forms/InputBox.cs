447dfa2679784baffe169abd1a6432d24ce54336
Timwi <github@timwi.de>
2009-06-02 15:13:54 +0100

    Provide InputBox method that lets you specify the caption of the OK and Cancel buttons (imported from Perforce changelist 931)

--- Src/Forms/InputBox.cs
+++ Src/Forms/InputBox.cs
@@ -21,35 +21,56 @@ namespace RT.Util.Dialogs
         }
 
         /// <summary>Prompts the user for input.</summary>
-        /// <param name="Prompt">Message to display to prompt the user.</param>
+        /// <param name="prompt">Message to display to prompt the user.</param>
         /// <returns>The text entered by the user, or null if the user selected the Cancel button.</returns>
-        public static string GetLine(string Prompt)
+        public static string GetLine(string prompt)
         {
-            return GetLine(Prompt, "", "Please enter text");
+            return GetLine(prompt, "", "Please enter text");
         }
 
         /// <summary>Prompts the user for input.</summary>
-        /// <param name="Prompt">Message to display to prompt the user.</param>
-        /// <param name="Default">Initial value to populate the input box with.</param>
+        /// <param name="prompt">Message to display to prompt the user.</param>
+        /// <param name="default">Initial value to populate the input box with.</param>
         /// <returns>The text entered by the user, or null if the user selected the Cancel button.</returns>
-        public static string GetLine(string Prompt, string Default)
+        public static string GetLine(string prompt, string @default)
         {
-            return GetLine(Prompt, Default, "Please enter text");
+            return GetLine(prompt, @default, "Please enter text");
         }
 
         /// <summary>Prompts the user for input.</summary>
-        /// <param name="Prompt">Message to display to prompt the user.</param>
-        /// <param name="Default">Initial value to populate the input box with.</param>
-        /// <param name="Caption">Caption to use in the title bar of the dialog.</param>
+        /// <param name="prompt">Message to display to prompt the user.</param>
+        /// <param name="default">Initial value to populate the input box with.</param>
+        /// <param name="caption">Caption to use in the title bar of the dialog.</param>
         /// <returns>The text entered by the user, or null if the user selected the Cancel button.</returns>
-        public static string GetLine(string Prompt, string Default, string Caption)
+        public static string GetLine(string prompt, string @default, string caption)
         {
-            InputBox Dlg = new InputBox();
-            Dlg.Text = Caption;
-            Dlg.PromptLabel.Text = Prompt;
-            Dlg.EnterBox.Text = Default;
-            if (Dlg.ShowDialog() == DialogResult.OK)
-                return Dlg.EnterBox.Text;
+            InputBox dlg = new InputBox();
+            dlg.Text = caption;
+            dlg.PromptLabel.Text = prompt;
+            dlg.EnterBox.Text = @default;
+            if (dlg.ShowDialog() == DialogResult.OK)
+                return dlg.EnterBox.Text;
+            else
+                return null;
+        }
+
+        /// <summary>Prompts the user for input.</summary>
+        /// <param name="prompt">Message to display to prompt the user.</param>
+        /// <param name="default">Initial value to populate the input box with.</param>
+        /// <param name="caption">Caption to use in the title bar of the dialog.</param>
+        /// <param name="okButtonText">Text to use on the OK button.</param>
+        /// <param name="cancelButtonText">Text to use on the Cancel button.</param>
+        /// <returns>The text entered by the user, or null if the user selected the Cancel button.</returns>
+        public static string GetLine(string prompt, string @default, string caption, string okButtonText, string cancelButtonText)
+        {
+            InputBox dlg = new InputBox();
+            dlg.Text = caption;
+            dlg.PromptLabel.Text = prompt;
+            dlg.EnterBox.Text = @default;
+            dlg.BtnOK.Text = okButtonText;
+            dlg.BtnCancel.Text = cancelButtonText;
+            if (dlg.ShowDialog() == DialogResult.OK)
+                return dlg.EnterBox.Text;
             else
                 return null;
         }
