d9584dc5499243080784e0e82b2caa55596dd70b
Timwi <github@timwi.de>
2009-06-23 23:09:19 +0100

    Remove resources file for NiceClosePanel - no need to have this just for a very small PNG file (imported from Perforce changelist 1010)

--- Src/Controls/NiceClosePanel.cs
+++ Src/Controls/NiceClosePanel.cs
@@ -1,8 +1,7 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows.Forms;
 using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
 
 namespace RT.Util.Controls
 {
@@ -19,13 +18,31 @@ namespace RT.Util.Controls
         public NiceClosePanel()
             : base()
         {
-            initializeComponent();
-            this.Resize += new EventHandler(resize);
-            this.Paint += new PaintEventHandler(paint);
-            this._closeButton.Click += new EventHandler(closeButton_Click);
+            _closeButton = new System.Windows.Forms.Button();
+            _closeButton.Name = "_closeButton";
+            _closeButton.Size = new System.Drawing.Size(8, 8);
+            _closeButton.Text = "X";
+            _closeButton.Tag = "notranslate";
+            _closeButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            // This is a small 8×8 PNG that contains a black X on a transparent background
+            _closeButton.Image = new Bitmap(new MemoryStream(new byte[] { 137, 80, 78, 71, 13, 10, 26, 10, 0, 0, 0, 13, 73, 72, 68, 82, 0, 0, 0, 8, 0, 0, 0, 8, 8, 6, 0, 0, 0, 196, 15, 190, 139, 0, 0, 0, 89, 73, 68, 65, 84, 40, 83, 99, 96, 128, 0, 126, 40, 141, 76, 113, 194, 56, 32, 198, 109, 32, 246, 64, 146, 53, 0, 178, 239, 1, 177, 16, 76, 204, 24, 200, 120, 2, 85, 4, 146, 124, 142, 166, 1, 172, 14, 164, 232, 37, 16, 191, 195, 38, 9, 82, 0, 210, 249, 6, 138, 145, 173, 3, 235, 70, 54, 22, 217, 58, 176, 36, 27, 16, 95, 69, 51, 22, 164, 8, 197, 145, 112, 47, 33, 249, 4, 44, 6, 0, 149, 131, 14, 219, 10, 117, 71, 99, 0, 0, 0, 0, 73, 69, 78, 68, 174, 66, 96, 130 }));
+            _closeButton.Margin = new System.Windows.Forms.Padding(0);
+            _closeButton.Padding = new System.Windows.Forms.Padding(0, 0, 2, 2);
+            _closeButton.TabIndex = 0;
+            _closeButton.TabStop = false;
+
+            Controls.Add(_closeButton);
+            Resize += new EventHandler(resize);
+            Paint += new PaintEventHandler(paint);
+            _closeButton.Click += new EventHandler(fireCloseClicked);
+
+            // This is a workaround which appears to be necessary in Mono. Otherwise the button does not appear until the first time the level list is resized.
+            Timer t = new Timer { Interval = 100 };
+            t.Tick += (s, e) => { resize(s, e); t.Enabled = false; };
+            t.Enabled = true;
         }
 
-        private void closeButton_Click(object sender, EventArgs e)
+        private void fireCloseClicked(object sender, EventArgs e)
         {
             if (CloseClicked != null)
                 CloseClicked(this, e);
@@ -46,31 +63,8 @@ namespace RT.Util.Controls
         {
             _closeButton.Size = new Size(ClientSize.Height, ClientSize.Height);
             _closeButton.Location = new Point(ClientSize.Width - _closeButton.Width, 0);
+            // MessageBox.Show("" + _closeButton.Size.ToString() + " -/- " + _closeButton.Location.ToString());
             Refresh();
-        }
-
-        private void initializeComponent()
-        {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NiceClosePanel));
-            this._closeButton = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // FCloseButton
-            // 
-            this._closeButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this._closeButton.Image = ((System.Drawing.Image) (resources.GetObject("FCloseButton.Image")));
-            this._closeButton.Location = new System.Drawing.Point(0, 0);
-            this._closeButton.Margin = new System.Windows.Forms.Padding(0);
-            this._closeButton.Name = "FCloseButton";
-            this._closeButton.Padding = new System.Windows.Forms.Padding(0, 0, 2, 2);
-            this._closeButton.Size = new System.Drawing.Size(8, 8);
-            this._closeButton.TabIndex = 0;
-            this._closeButton.TabStop = false;
-            // 
-            // NiceClosePanel
-            // 
-            this.Controls.Add(this._closeButton);
-            this.ResumeLayout(false);
         }
     }
 }
