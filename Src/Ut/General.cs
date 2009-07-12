c03d0e7c394e16c654df8d7fbaec2131ae63188e
rstarkov <sourcecode@roman.st>
2009-07-12 12:48:12 +0100

    Added Ut.Min and Ut.Max methods. (imported from Perforce changelist 1075)

--- Src/Ut/General.cs
+++ Src/Ut/General.cs
@@ -31,5 +31,61 @@ namespace RT.Util
             else
                 return (size / (1024d * 1024d * 1024d)).ToString("#,###.## GB");
         }
+
+        /// <summary>
+        /// Returns the smaller of the two IComparable values. If the values are
+        /// equal, returns the first one.
+        /// </summary>
+        public static T Min<T>(T val1, T val2) where T : IComparable<T>
+        {
+            return val1.CompareTo(val2) <= 0 ? val1 : val2;
+        }
+
+        /// <summary>
+        /// Returns the smaller of the two IComparable values. If the values are
+        /// equal, returns the first one.
+        /// </summary>
+        public static T Min<T>(T val1, T val2, T val3) where T : IComparable<T>
+        {
+            T c1 = val1.CompareTo(val2) <= 0 ? val1 : val2;
+            return c1.CompareTo(val3) <= 0 ? c1 : val3;
+        }
+
+        /// <summary>
+        /// Returns the smallest of all arguments passed in. Uses the Linq .Min
+        /// extension method to do the work.
+        /// </summary>
+        public static T Min<T>(params T[] args) where T : IComparable<T>
+        {
+            return args.Min();
+        }
+
+        /// <summary>
+        /// Returns the larger of the two IComparable values. If the values are
+        /// equal, returns the first one.
+        /// </summary>
+        public static T Max<T>(T val1, T val2) where T : IComparable<T>
+        {
+            return val1.CompareTo(val2) >= 0 ? val1 : val2;
+        }
+
+        /// <summary>
+        /// Returns the larger of the two IComparable values. If the values are
+        /// equal, returns the first one.
+        /// </summary>
+        public static T Max<T>(T val1, T val2, T val3) where T : IComparable<T>
+        {
+            T c1 = val1.CompareTo(val2) >= 0 ? val1 : val2;
+            return c1.CompareTo(val3) >= 0 ? c1 : val3;
+        }
+
+        /// <summary>
+        /// Returns the largest of all arguments passed in. Uses the Linq .Max
+        /// extension method to do the work.
+        /// </summary>
+        public static T Max<T>(params T[] args) where T : IComparable<T>
+        {
+            return args.Max();
+        }
     }
 }
