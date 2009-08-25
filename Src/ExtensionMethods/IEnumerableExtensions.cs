ce8c60a561d4c28373234172396662329869b727
Timwi <github@timwi.de>
2009-08-25 18:00:11 +0100

    Fix bugs where enumerables were evaluated more than once (imported from Perforce changelist 1187)

--- Src/ExtensionMethods/IEnumerableExtensions.cs
+++ Src/ExtensionMethods/IEnumerableExtensions.cs
@@ -25,8 +25,10 @@ namespace RT.Util.ExtensionMethods
         /// </summary>
         public static IEnumerable<Tuple<T, U>> Join<T, U>(this IEnumerable<T> source, IEnumerable<U> with)
         {
+            // Make sure that 'with' is evaluated only once
+            U[] withArr = with.ToArray();
             foreach (T item1 in source)
-                foreach (U item2 in with)
+                foreach (U item2 in withArr)
                     yield return new Tuple<T, U>(item1, item2);
         }
 
@@ -36,19 +38,11 @@ namespace RT.Util.ExtensionMethods
         /// </summary>
         public static IEnumerable<Tuple<T, T>> UniquePairs<T>(this IEnumerable<T> source)
         {
-            int i = 0;
-            foreach (T item1 in source)
-            {
-                i++;
-                int j = 0;
-                foreach (T item2 in source)
-                {
-                    j++;
-                    if (j <= i)
-                        continue;
-                    yield return new Tuple<T, T>(item1, item2);
-                }
-            }
+            // Make sure that 'source' is evaluated only once
+            T[] arr = source.ToArray();
+            for (int i = 0; i < arr.Length - 1; i++)
+                for (int j = i + 1; j < arr.Length; j++)
+                    yield return new Tuple<T, T>(arr[i], arr[j]);
         }
 
         /// <summary>
