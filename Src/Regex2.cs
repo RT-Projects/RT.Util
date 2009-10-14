69971ff4e2979c5320e2318d38e0798fa42f0480
rstarkov <sourcecode@roman.st>
2009-10-14 20:10:28 +0100

    Conversions to/from base32, Regex2 (imported from Perforce changelist 1303)

---
+++ Src/Regex2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace R.Util
+{
+    public class Regex2
+    {
+        public static List<Match> Matches(string input, string pattern)
+        {
+            return new List<Match>(Regex.Matches(input, pattern, RegexOptions.Singleline).Cast<Match>());
+        }
+
+        public static List<Match> Matches(string input, string pattern, RegexOptions options)
+        {
+            return new List<Match>(Regex.Matches(input, pattern, options | RegexOptions.Singleline).Cast<Match>());
+        }
+
+        public static Match Match(string input, string pattern)
+        {
+            return Regex.Match(input, pattern, RegexOptions.Singleline);
+        }
+
+        public static Match Match(string input, string pattern, RegexOptions options)
+        {
+            return Regex.Match(input, pattern, options | RegexOptions.Singleline);
+        }
+    }
+}
