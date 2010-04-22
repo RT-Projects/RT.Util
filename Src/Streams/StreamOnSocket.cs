ef9451e0103e7a5a6e6c316bb6cf7d1a533c9a2d
Timwi <github@timwi.de>
2010-04-22 22:03:56 +0100

    Rename StreamOnSocket to SocketWriterStream (because it's write-only) and introduce a SocketReaderStream (imported from Perforce changelist 1888)

--- Src/Streams/StreamOnSocket.cs
+++ Src/Streams/StreamOnSocket.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Net.Sockets;
 using System.Text;
 
@@ -7,16 +8,16 @@ namespace RT.Util.Streams
     /// <summary>
     /// Use this if you need to write to a <see cref="Stream"/> but actually want the output sent to a <see cref="System.Net.Sockets.Socket"/>.
     /// </summary>
-    public class StreamOnSocket : Stream
+    public class SocketWriterStream : Stream
     {
         /// <summary>Contains the underlying socket.</summary>
         protected Socket Socket;
 
         /// <summary>
-        /// Constructs a <see cref="StreamOnSocket"/> object that can send output to a <see cref="System.Net.Sockets.Socket"/>.
+        /// Constructs a <see cref="SocketWriterStream"/> object that can send output to a <see cref="System.Net.Sockets.Socket"/>.
         /// </summary>
         /// <param name="socket">The socket to write all the output to.</param>
-        public StreamOnSocket(Socket socket)
+        public SocketWriterStream(Socket socket)
         {
             this.Socket = socket;
         }
@@ -51,20 +52,20 @@ namespace RT.Util.Streams
         public override void SetLength(long value) { throw new System.NotSupportedException(); }
 
 #pragma warning restore 1591    // Missing XML comment for publicly visible type or member
-    
+
     }
 
     /// <summary>
-    /// Same as <see cref="StreamOnSocket"/>, but performs the HTTP Transfer-Encoding: chunked.
+    /// Same as <see cref="SocketWriterStream"/>, but performs the HTTP Transfer-Encoding: chunked.
     /// </summary>
-    public sealed class StreamOnSocketChunked : StreamOnSocket
+    public sealed class ChunkedSocketWriterStream : SocketWriterStream
     {
         /// <summary>
-        /// Constructs a <see cref="StreamOnSocketChunked"/> object that can send output to a
+        /// Constructs a <see cref="ChunkedSocketWriterStream"/> object that can send output to a
         /// <see cref="Socket"/> in HTTP "chunked" Transfer-Encoding.
         /// </summary>
         /// <param name="socket">The socket to write all the output to.</param>
-        public StreamOnSocketChunked(Socket socket)
+        public ChunkedSocketWriterStream(Socket socket)
             : base(socket)
         {
             Socket = socket;
@@ -84,12 +85,136 @@ namespace RT.Util.Streams
         }
 
         /// <summary>
-        /// Closes this <see cref="StreamOnSocketChunked"/>. It is important that this is called
+        /// Closes this <see cref="ChunkedSocketWriterStream"/>. It is important that this is called
         /// because it outputs the trailing null chunk to the socket, indicating the end of the data.
         /// </summary>
         public override void Close()
         {
             Socket.Send(new byte[] { (byte) '0', 13, 10, 13, 10 }); // "0\r\n\r\n"
+        }
+    }
+
+    /// <summary>
+    /// Use this if you need to read from a <see cref="Stream"/> but actually want the input read from a <see cref="System.Net.Sockets.Socket"/>.
+    /// </summary>
+    public sealed class SocketReaderStream : Stream
+    {
+        private Socket _socket;
+        private long _maxBytesToRead;
+        private byte[] _lastRead;
+        private int _lastReadOffset;
+        private int _lastReadCount;
+
+        /// <summary>
+        /// Constructs a <see cref="SocketReaderStream"/> object that can read input from a <see cref="System.Net.Sockets.Socket"/>.
+        /// </summary>
+        /// <param name="socket">The socket to read the input from.</param>
+        /// <param name="maxBytesToRead">Maximum number of bytes to read from the socket. After this, the stream pretends to have reached the end.</param>
+        public SocketReaderStream(Socket socket, long maxBytesToRead)
+        {
+            _socket = socket;
+            _maxBytesToRead = maxBytesToRead;
+            _lastRead = null;
+            _lastReadOffset = 0;
+            _lastReadCount = 0;
+        }
+
+        /// <summary>
+        /// Constructs a <see cref="SocketReaderStream"/> object that reads from a given bit of initial data, and then continues on to read from a <see cref="System.Net.Sockets.Socket"/> 
+        /// </summary>
+        /// <param name="socket">The socket to read the input from.</param>
+        /// <param name="maxBytesToRead">Maximum number of bytes to read from the initial data plus the socket. After this, the stream pretends to have reached the end.</param>
+        /// <param name="initialBuffer">Buffer containing the initial data. The buffer is not copied, so make sure you don't modify the contents of the buffer before it is consumed by reading.</param>
+        /// <param name="initialBufferOffset">Offset into the buffer where the initial data starts.</param>
+        /// <param name="initialBufferCount">Number of bytes of initial data in the buffer.</param>
+        public SocketReaderStream(Socket socket, long maxBytesToRead, byte[] initialBuffer, int initialBufferOffset, int initialBufferCount)
+        {
+            _socket = socket;
+
+            if (initialBufferCount <= 0)
+            {
+                _lastRead = null;
+                _lastReadOffset = 0;
+                _lastReadCount = 0;
+                _maxBytesToRead = maxBytesToRead;
+            }
+            else if (initialBufferCount > maxBytesToRead)
+            {
+                _lastRead = initialBuffer;
+                _lastReadOffset = initialBufferOffset;
+                // The conversion to int here is safe because we know it's smaller than initialBufferCount, which is an int
+                _lastReadCount = (int) maxBytesToRead;  
+                _maxBytesToRead = 0;
+            }
+            else
+            {
+                _lastRead = initialBuffer;
+                _lastReadOffset = initialBufferOffset;
+                _lastReadCount = initialBufferCount;
+                _maxBytesToRead = maxBytesToRead - initialBufferCount;
+            }
+        }
+
+#pragma warning disable 1591    // Missing XML comment for publicly visible type or member
+        public override bool CanRead { get { return true; } }
+        public override bool CanSeek { get { return false; } }
+        public override bool CanWrite { get { return false; } }
+        public override void Flush() { }
+        public override long Length { get { throw new NotSupportedException(); } }
+        public override long Position { get { throw new NotSupportedException(); } set { throw new NotSupportedException(); } }
+        public override long Seek(long offset, SeekOrigin origin) { throw new NotSupportedException(); }
+        public override void SetLength(long value) { throw new NotSupportedException(); }
+        public override void Write(byte[] buffer, int offset, int count) { throw new NotSupportedException(); }
+#pragma warning restore 1591    // Missing XML comment for publicly visible type or member
+
+        /// <summary>Reads up to the specified number of bytes from the underlying socket.</summary>
+        /// <param name="buffer">Buffer to write received data into.</param>
+        /// <param name="offset">Offset into the buffer where to start writing.</param>
+        /// <param name="count">Maximum number of bytes in the buffer to write to.</param>
+        /// <returns>Number of bytes actually written to the buffer.</returns>
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            // If we have something left from the last socket-receive operation, return as much of that as possible
+            if (_lastRead != null)
+            {
+                if (count >= _lastReadCount)
+                {
+                    Buffer.BlockCopy(_lastRead, _lastReadOffset, buffer, offset, _lastReadCount);
+                    _lastRead = null;
+                    return _lastReadCount;
+                }
+                else
+                {
+                    Buffer.BlockCopy(_lastRead, _lastReadOffset, buffer, offset, count);
+                    _lastReadOffset += count;
+                    _lastReadCount -= count;
+                    return count;
+                }
+            }
+            else
+            {
+                // If we have read as many bytes as we are supposed to, simulate the end of the stream
+                if (_maxBytesToRead <= 0)
+                    return 0;
+
+                // Read at most _maxBytesToRead bytes from the socket
+                var size = (int) Math.Min(65536, _maxBytesToRead);
+                _lastRead = new byte[size];
+                _lastReadOffset = 0;
+                _lastReadCount = _socket.Receive(_lastRead, _lastReadOffset, size, SocketFlags.None);
+                _maxBytesToRead -= _lastReadCount;
+
+                // Socket error?
+                if (_lastReadCount == 0)
+                {
+                    _lastRead = null;
+                    _maxBytesToRead = 0;
+                    return 0;
+                }
+
+                // We've populated _lastRead; use a tail-recursive call to actually return the data
+                return Read(buffer, offset, count);
+            }
         }
     }
 }
