5b5ae2a808ac3bb08e45c50f9e9a9837b54827e4
Timwi <github@timwi.de>
2010-05-06 16:46:17 +0100

    * Move the code to execute the post-build checks from Util to PostBuildRunner * Document CustomComparer<T> (imported from Perforce changelist 1971)

--- Src/Collections/Comparer.cs
+++ Src/Collections/Comparer.cs
@@ -5,15 +5,25 @@ using System.Text;
 
 namespace RT.Util.Collections
 {
+    /// <summary>Encapsulates an IComparer&lt;T&gt; that uses a comparison function provided as a delegate.</summary>
+    /// <typeparam name="T">The type of elements to be compared.</typeparam>
     public class CustomComparer<T> : IComparer<T>
     {
         private Comparison<T> _comparison;
+        /// <summary>Constructor.</summary>
+        /// <param name="comparison">Provides the comparison function for this comparer.</param>
         public CustomComparer(Comparison<T> comparison) { _comparison = comparison; }
+        /// <summary>Compares two elements.</summary>
+        /// <remarks>This method implements <see cref="IComparer&lt;T&gt;.Compare(T,T)"/>.</remarks>
         public int Compare(T x, T y) { return _comparison(x, y); }
     }
 
+    /// <summary>Provides a static method to create a <see cref="CustomComparer&lt;T&gt;"/> from a delegate.</summary>
     public static class CustomComparer
     {
+        /// <summary>Creates an instance of <see cref="CustomComparer&lt;T&gt;"/> using the specified delegate.</summary>
+        /// <typeparam name="T">The type of elements to be compared.</typeparam>
+        /// <param name="comparison">Provides the comparison function for this comparer.</param>
         public static CustomComparer<T> Create<T>(Comparison<T> comparison)
         {
             return new CustomComparer<T>(comparison);
