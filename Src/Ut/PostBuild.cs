b275e128362f0e02d22e3ffe8195f2d23a381125
Timwi <github@timwi.de>
2010-05-06 21:12:08 +0100

    Put the documentation improvement back in (accidentally reverted in 1976) (imported from Perforce changelist 1977)

--- Src/Ut/PostBuild.cs
+++ Src/Ut/PostBuild.cs
@@ -145,12 +145,14 @@ namespace RT.Util
     /// <summary>Provides the ability to output post-build messages (with filename and line number) to Console.Error. This interface is used by <see cref="Ut.RunPostBuildChecks"/>.</summary>
     public interface IPostBuildReporter
     {
-        /// <summary>When implemented in a class, finds the first occurrence of the first token in <paramref name="tokens"/>, and then starts searching there to find the first occurrence of each of the subsequent
-        /// <paramref name="tokens"/> within the same file. When found, outputs the error <paramref name="message"/> including the filename and line number where the last token was found.</summary>
+        /// <summary>When implemented in a class, searches the source directory for the first occurrence of the first token in <paramref name="tokens"/>,
+        /// and then starts searching there to find the first occurrence of each of the subsequent <paramref name="tokens"/> within the same file. When found,
+        /// outputs the error <paramref name="message"/> including the filename and line number where the last token was found.</summary>
         void Error(string message, params string[] tokens);
 
-        /// <summary>When implemented in a class, finds the first occurrence of the first token in <paramref name="tokens"/>, and then starts searching there to find the first occurrence of each of the subsequent
-        /// <paramref name="tokens"/> within the same file. When found, outputs the warning <paramref name="message"/> including the filename and line number where the last token was found.</summary>
+        /// <summary>When implemented in a class, searches the source directory for the first occurrence of the first token in <paramref name="tokens"/>,
+        /// and then starts searching there to find the first occurrence of each of the subsequent <paramref name="tokens"/> within the same file. When found,
+        /// outputs the warning <paramref name="message"/> including the filename and line number where the last token was found.</summary>
         void Warning(string message, params string[] tokens);
     }
 #endif
