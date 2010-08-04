4ba9bd57f1c8e99aea9271562f64c45f6ec1acd5
rstarkov <sourcecode@roman.st>
2010-08-04 15:40:30 +0100

    Forgot to submit attribute changes. (imported from Perforce changelist 2123)

--- Src/RummageAttributes.cs
+++ Src/RummageAttributes.cs
@@ -5,51 +5,52 @@ using System.Text;
 
 namespace RT.Util
 {
+    static class AttributeTargetSets
+    {
+        public const AttributeTargets TypeDefinitions = AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Enum | AttributeTargets.Delegate | AttributeTargets.Interface;
+        public const AttributeTargets DefinitionsWithNames = AttributeTargetSets.TypeDefinitions | AttributeTargets.Method | AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Event | AttributeTargets.Parameter | AttributeTargets.GenericParameter;
+        public const AttributeTargets DefinitionsWithAccessModifiers = AttributeTargetSets.TypeDefinitions | AttributeTargets.Constructor | AttributeTargets.Method | AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Event;
+    }
+
     /// <summary>Instructs Rummage to keep a specific type, method, constructor or field.</summary>
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Enum | AttributeTargets.Delegate | AttributeTargets.Interface | AttributeTargets.Constructor | AttributeTargets.Method | AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
+    [AttributeUsage(AttributeTargetSets.TypeDefinitions | AttributeTargets.Constructor | AttributeTargets.Method | AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
     public sealed class RummageNoRemoveAttribute : Attribute
     {
-        /// <summary>Constructor.</summary>
-        public RummageNoRemoveAttribute() { }
     }
 
     /// <summary>Instructs Rummage to keep the original name of a specific element. </summary>
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Enum | AttributeTargets.Constructor | AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Event | AttributeTargets.Interface | AttributeTargets.Parameter | AttributeTargets.Delegate | AttributeTargets.GenericParameter, Inherited = false, AllowMultiple = false)]
+    [AttributeUsage(AttributeTargetSets.DefinitionsWithNames, Inherited = false, AllowMultiple = false)]
     public sealed class RummageNoRenameAttribute : Attribute
     {
-        /// <summary>Constructor.</summary>
-        public RummageNoRenameAttribute() { }
     }
 
-    /// <summary>Instructs Rummage to keep all the methods, constructors and fields in a specific type.</summary>
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Enum | AttributeTargets.Delegate | AttributeTargets.Interface, Inherited = false, AllowMultiple = false)]
-    public sealed class RummageNoRemoveAnythingAttribute : Attribute
+    [AttributeUsage(AttributeTargetSets.TypeDefinitions, Inherited = false, AllowMultiple = false)]
+    public sealed class RummageNoUnnestAttribute : Attribute
     {
-        /// <summary>Constructor.</summary>
-        public RummageNoRemoveAnythingAttribute() { }
     }
 
-    /// <summary>Instructs Rummage to keep the original name of all the methods, fields and nested types within a specific type.</summary>
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Enum | AttributeTargets.Delegate | AttributeTargets.Interface, Inherited = false, AllowMultiple = false)]
-    public sealed class RummageNoRenameAnythingAttribute : Attribute
+    [AttributeUsage(AttributeTargetSets.DefinitionsWithAccessModifiers, Inherited = false, AllowMultiple = false)]
+    public sealed class RummageNoMarkPublicAttribute : Attribute
     {
-        /// <summary>Constructor.</summary>
-        public RummageNoRenameAnythingAttribute() { }
-    }
-
-    /// <summary>Instructs Rummage to inline a specific method that would otherwise not be automatically inlined.</summary>
-    [AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
-    public sealed class RummageInlineAttribute : Attribute
-    {
-        /// <summary>Constructor.</summary>
-        public RummageInlineAttribute() { }
     }
 
     /// <summary>Instructs Rummage not to inline a specific method that would otherwise be automatically inlined. This attribute takes precedence over <see cref="RummageInlineAttribute"/> if both are specified on the same method.</summary>
     [AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
     public sealed class RummageNoInlineAttribute : Attribute
     {
-        /// <summary>Constructor.</summary>
-        public RummageNoInlineAttribute() { }
+    }
+
+    /// <summary>Instructs Rummage to inline a specific method that would otherwise not be automatically inlined.</summary>
+    [AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
+    public sealed class RummageInlineAttribute : Attribute
+    {
+    }
+
+
+
+    /// <summary>Instructs Rummage to keep all the methods, constructors and fields in a specific type.</summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Enum | AttributeTargets.Delegate | AttributeTargets.Interface | AttributeTargets.Constructor, Inherited = false, AllowMultiple = false)]
+    public sealed class RummageKeepReflectionSafeAttribute : Attribute
+    {
     }
 }
