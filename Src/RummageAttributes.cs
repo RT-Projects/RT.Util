ffc4f3936a0503e02afad0f6686c7c1a34211482
Timwi <github@timwi.de>
2010-12-01 19:03:42 +0000

    Rummage custom attributes tweaked. (imported from Perforce changelist 2540)

--- Src/RummageAttributes.cs
+++ Src/RummageAttributes.cs
@@ -13,37 +13,37 @@ namespace RT.Util
     }
 
     /// <summary>Instructs Rummage to keep a specific type, method, constructor or field.</summary>
-    [AttributeUsage(AttributeTargetSets.TypeDefinitions | AttributeTargets.Constructor | AttributeTargets.Method | AttributeTargets.Field, Inherited = false, AllowMultiple = false), RummageKeepReflectionSafe]
+    [AttributeUsage(AttributeTargetSets.TypeDefinitions | AttributeTargets.Constructor | AttributeTargets.Method | AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
     public sealed class RummageNoRemoveAttribute : Attribute
     {
     }
 
     /// <summary>Instructs Rummage to keep the original name of a specific element. </summary>
-    [AttributeUsage(AttributeTargetSets.DefinitionsWithNames, Inherited = false, AllowMultiple = false), RummageKeepReflectionSafe]
+    [AttributeUsage(AttributeTargetSets.DefinitionsWithNames, Inherited = false, AllowMultiple = false)]
     public sealed class RummageNoRenameAttribute : Attribute
     {
     }
 
     /// <summary>Instructs rummage to avoid un-nesting the specified type.</summary>
-    [AttributeUsage(AttributeTargetSets.TypeDefinitions, Inherited = false, AllowMultiple = false), RummageKeepReflectionSafe]
+    [AttributeUsage(AttributeTargetSets.TypeDefinitions, Inherited = false, AllowMultiple = false)]
     public sealed class RummageNoUnnestAttribute : Attribute
     {
     }
 
     /// <summary>Instructs rummage to keep the original access modifier of a specific element.</summary>
-    [AttributeUsage(AttributeTargetSets.DefinitionsWithAccessModifiers, Inherited = false, AllowMultiple = false), RummageKeepReflectionSafe]
+    [AttributeUsage(AttributeTargetSets.DefinitionsWithAccessModifiers, Inherited = false, AllowMultiple = false)]
     public sealed class RummageNoMarkPublicAttribute : Attribute
     {
     }
 
     /// <summary>Instructs Rummage not to inline a specific method or property that would otherwise be automatically inlined. This attribute takes precedence over <see cref="RummageInlineAttribute"/> if both are specified on the same method or property.</summary>
-    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property, Inherited = false, AllowMultiple = false), RummageKeepReflectionSafe]
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
     public sealed class RummageNoInlineAttribute : Attribute
     {
     }
 
     /// <summary>Instructs Rummage to inline a specific method or property that would otherwise not be automatically inlined.</summary>
-    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property, Inherited = false, AllowMultiple = false), RummageKeepReflectionSafe]
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
     public sealed class RummageInlineAttribute : Attribute
     {
     }
@@ -51,19 +51,19 @@ namespace RT.Util
 
 
     /// <summary>Instructs Rummage to refrain from making any changes to a specific type.</summary>
-    [AttributeUsage(AttributeTargetSets.DefinitionsWithAccessModifiers | AttributeTargetSets.DefinitionsWithNames | AttributeTargetSets.TypeDefinitions, Inherited = false, AllowMultiple = false), RummageKeepUsersReflectionSafe, RummageKeepReflectionSafe]
+    [AttributeUsage(AttributeTargetSets.DefinitionsWithAccessModifiers | AttributeTargetSets.DefinitionsWithNames | AttributeTargetSets.TypeDefinitions, Inherited = false, AllowMultiple = false), RummageKeepUsersReflectionSafe]
     public sealed class RummageKeepReflectionSafeAttribute : Attribute
     {
     }
 
     /// <summary>Instructs Rummage to keep all the types reflection-safe which are passed in for the given generic parameter.</summary>
-    [AttributeUsage(AttributeTargets.GenericParameter, Inherited = false, AllowMultiple = false), RummageKeepReflectionSafe]
+    [AttributeUsage(AttributeTargets.GenericParameter, Inherited = false, AllowMultiple = false)]
     public sealed class RummageKeepArgumentsReflectionSafeAttribute : Attribute
     {
     }
 
     /// <summary>Use only on custom-attribute class declarations. Instructs Rummage to keep everything reflection-safe that uses the given custom attribute.</summary>
-    [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false), RummageKeepReflectionSafe]
+    [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
     public sealed class RummageKeepUsersReflectionSafeAttribute : Attribute
     {
     }
