5f2f22f3d8fed620999bddcb1bd997db20efd2e5
rstarkov <sourcecode@roman.st>
2011-07-12 17:37:10 +0100

    RTSmtpClient: support SSL (imported from Perforce changelist 2963)

--- Src/RTSmtpClient.cs
+++ Src/RTSmtpClient.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Mail;
+using System.Net.Security;
 using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Text.RegularExpressions;
 using RT.Util.ExtensionMethods;
@@ -11,7 +13,6 @@ using RT.Util.ExtensionMethods;
 namespace RT.Util
 {
     /// <summary>Encapsulates an error condition that occurred during an SMTP exchange.</summary>
-    [Serializable]
     public sealed class RTSmtpException : RTException
     {
         /// <summary>Contains the SMTP conversation (protocol text sent back and forth) up to the point of the error.</summary>
@@ -27,11 +28,23 @@ namespace RT.Util
         }
     }
 
+    /// <summary>Represents an SMTP encryption mode.</summary>
+    public enum SmtpEncryption
+    {
+        /// <summary>No encryption</summary>
+        None,
+        /// <summary>SSL/TLS encryption</summary>
+        Ssl,
+        /// <summary>SSL/TLS encryption; any certificate validation errors are ignored.</summary>
+        SslIgnoreCert,
+    }
+
     /// <summary>Provides methods to send e-mails via an SMTP server.</summary>
     public sealed class RTSmtpClient : IDisposable
     {
         private TcpClient _tcp;
         private Stream _tcpStream;
+        private SslStream _sslStream;
         private TextWriter _writer;
         private TextReader _reader;
         private List<string> _conversation;
@@ -41,13 +54,22 @@ namespace RT.Util
         /// <param name="port">SMTP host port.</param>
         /// <param name="username">SMTP username.</param>
         /// <param name="password">SMTP password.</param>
+        /// <param name="encryption">Encryption mode.</param>
         /// <exception cref="RTSmtpException">SMTP protocol error, or authentication failed.</exception>
-        public RTSmtpClient(string host, int port, string username, string password)
+        public RTSmtpClient(string host, int port, string username, string password, SmtpEncryption encryption = SmtpEncryption.None)
         {
             _tcp = new TcpClient(host, port);
             _tcpStream = _tcp.GetStream();
-            _writer = new StreamWriter(_tcpStream, new UTF8Encoding(false)) { NewLine = "\r\n", AutoFlush = true };
-            _reader = new StreamReader(_tcpStream, Encoding.UTF8);
+            if (encryption == SmtpEncryption.Ssl || encryption == SmtpEncryption.SslIgnoreCert)
+            {
+                if (encryption == SmtpEncryption.Ssl)
+                    _sslStream = new SslStream(_tcpStream);
+                else
+                    _sslStream = new SslStream(_tcpStream, false, (object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) => true);
+                _sslStream.AuthenticateAsClient(host);
+            }
+            _writer = new StreamWriter(_sslStream ?? _tcpStream, new UTF8Encoding(false)) { NewLine = "\r\n", AutoFlush = true };
+            _reader = new StreamReader(_sslStream ?? _tcpStream, Encoding.UTF8);
             _conversation = new List<string>();
 
             sendAndExpect(null, 220);
@@ -254,6 +276,8 @@ namespace RT.Util
                 ((IDisposable) _tcp).Dispose();
             if (_tcpStream != null)
                 ((IDisposable) _tcpStream).Dispose();
+            if (_sslStream != null)
+                ((IDisposable) _sslStream).Dispose();
             if (_writer != null)
                 ((IDisposable) _writer).Dispose();
             if (_reader != null)
