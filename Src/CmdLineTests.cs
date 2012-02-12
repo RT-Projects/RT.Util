22708ea5d37e4ff946aa4cc6414402279850934c
rstarkov <sourcecode@roman.st>
2012-02-12 11:16:03 +0000

    A test for CmdLine showing how it swallows -- where it shouldn't. (imported from Perforce changelist 3513)

---
+++ Src/CmdLineTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using RT.Util.CommandLine;
+
+namespace RT.Util
+{
+    [TestFixture]
+    public sealed class CmdLineTests
+    {
+        private class commandLine
+        {
+            [Option("--stuff")]
+            public string Stuff;
+            [IsPositional]
+            public string[] Args;
+        }
+
+        [Test]
+        public static void Test()
+        {
+            var c = CommandLineParser<commandLine>.Parse("--stuff blah abc def".Split(' '));
+            Assert.AreEqual("blah", c.Stuff);
+            Assert.IsTrue(c.Args.SequenceEqual(new[] { "abc", "def" }));
+
+            c = CommandLineParser<commandLine>.Parse("def --stuff thingy abc".Split(' '));
+            Assert.AreEqual("thingy", c.Stuff);
+            Assert.IsTrue(c.Args.SequenceEqual(new[] { "def", "abc" }));
+
+            c = CommandLineParser<commandLine>.Parse("--stuff stuff -- abc --stuff blah -- def".Split(' '));
+            Assert.AreEqual("stuff", c.Stuff);
+            Assert.IsTrue(c.Args.SequenceEqual(new[] { "abc", "--stuff", "blah", "--", "def" }));
+        }
+    }
+}
