6813e69e17f31b19d74cdf42191bcb363979b586
Timwi <github@timwi.de>
2012-03-23 14:34:56 +0000

    - Move NameValuesCollection from RT.Util to RT.Servers because it is only ever used there - Add NameGenerator to Util (imported from Perforce changelist 3620)

---
+++ Src/NameGenerator.cs
@@ -0,0 +1,41 @@
+﻿
+namespace RT.Util
+{
+    /// <summary>Generates a sequence of short names starting with “a” through “z”, followed by two-letter names etc.</summary>
+    public sealed class NameGenerator
+    {
+        /// <summary>Constructor.</summary>
+        public NameGenerator() { _curname = ""; }
+
+        /// <summary>Generates the next name in the sequence.</summary>
+        public string NextName()
+        {
+            _curname = nextName(_curname);
+            return _curname;
+        }
+
+        private string _curname;
+
+        private static string nextName(string prev)
+        {
+            if (string.IsNullOrEmpty(prev))
+                return "a";
+            if (prev.Length == 1)
+            {
+                if (prev[0] == 'z')
+                    return "aa";
+                else
+                    return ((char) (prev[0] + 1)).ToString();
+            }
+            else
+            {
+                if (prev[prev.Length - 1] == 'z')
+                    return prev.Substring(0, prev.Length - 1) + "0";
+                else if (prev[prev.Length - 1] == '9')
+                    return nextName(prev.Substring(0, prev.Length - 1)) + "a";
+                else
+                    return prev.Substring(0, prev.Length - 1) + (char) (prev[prev.Length - 1] + 1);
+            }
+        }
+    }
+}
