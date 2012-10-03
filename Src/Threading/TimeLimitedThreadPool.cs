7cd8e3adeaf244f486ca6b6abb7fcfbc6e6c638f
rstarkov <sourcecode@roman.st>
2012-10-03 15:31:19 +0100

    TimeLimitedThreadPool now uses an abstract base class for tasks. Bugfix for the aborted state not being set for tasks with no abort action. (imported from Perforce changelist 3911)

--- Src/Threading/TimeLimitedThreadPool.cs
+++ Src/Threading/TimeLimitedThreadPool.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 
+// Future ideas:
+// - make it possible to change the TimeLimit of an enqueued task (fairly easy, just need to wake up the control thread if the task has started and is due before the sleep ends)
+// - ExecuteTask helper method, for a single task, not even using the thread pool. Could return bool to indicate whether the task got completed or aborted.
+// - ExecuteTasks helper method. Could use the calling thread as the control thread.
+
 namespace RT.Util.Threading
 {
     /// <summary>
@@ -109,11 +114,10 @@ namespace RT.Util.Threading
                         {
                             worker.Thread.Abort();
                             if (worker.Task != null) // the occasional task will complete between the first check and the thread abort; no need to invoke the abort action for these since we know for sure they completed in their entirety
-                                if (task.AbortAction != null) // access via the task variable since the Task field could still disappear from under our feet
-                                {
-                                    task.AbortAction();
-                                    task.State = TimeLimitedTaskState.Aborted;
-                                }
+                            {
+                                task.State = TimeLimitedTaskState.Aborted;
+                                task.Aborted(); // access via the task variable since the Task field could still disappear from under our feet
+                            }
                             worker.Task = null; // mark that there's no task on this thread
                             createWorkerThread(worker);
                         }
@@ -163,7 +167,7 @@ namespace RT.Util.Threading
                 if (_controlSleepingUntil > worker.AbortTime)
                     _controlWakeup.Set();
 
-                worker.Task.TaskAction();
+                worker.Task.Execute();
 
                 var task = worker.Task;
                 worker.Task = null; // First mark the order as idle
@@ -259,14 +263,14 @@ namespace RT.Util.Threading
         /// and all tasks queued earlier are completed. If the pool was shut down, it will be started up automatically.</summary>
         public void EnqueueTask(TimeSpan timeLimit, Action action, Action abortAction = null)
         {
-            EnqueueTask(new TimeLimitedTask(timeLimit, action, abortAction));
+            EnqueueTask(new TimeLimitedDelegateTask(timeLimit, action, abortAction));
         }
 
         /// <summary>Places a number of time-limited tasks into the execution queue, which will start executing as soon as a worker is available
         /// and all tasks queued earlier are completed. If the pool was shut down, it will be started up automatically.</summary>
         public void EnqueueTasks(TimeSpan timeLimit, IEnumerable<Action> action, Action abortAction = null)
         {
-            EnqueueTasks(action.Select(a => new TimeLimitedTask(timeLimit, a, abortAction)));
+            EnqueueTasks(action.Select(a => new TimeLimitedDelegateTask(timeLimit, a, abortAction)));
         }
     }
 
@@ -287,34 +291,63 @@ namespace RT.Util.Threading
         Cancelled,
     }
 
-    /// <summary>
-    /// Encapsulates a time-limited task used with the <see cref="TimeLimitedThreadPool"/>.
-    /// </summary>
-    public class TimeLimitedTask
+    /// <summary>Encapsulates a time-limited task used with the <see cref="TimeLimitedThreadPool"/>.</summary>
+    public abstract class TimeLimitedTask
     {
-        /// <summary>The action invoked to perform the task in question. This code must be hardened against the asynchronous <see cref="ThreadAbortException"/>.</summary>
-        public Action TaskAction { get; private set; }
-        /// <summary>The action invoked if the task has been aborted due to time-out (but not cancelled or aborted due to a Shutdown).</summary>
-        public Action AbortAction { get; private set; }
         /// <summary>The maximum amount of time this task may take from the point when it begins executing. Tasks exceeding this limit are automatically aborted.</summary>
         public TimeSpan TimeLimit { get; private set; }
+        /// <summary>Invoked to execute this task. This code must be hardened against the asynchronous <see cref="ThreadAbortException"/>.</summary>
+        public abstract void Execute();
+        /// <summary>Invoked if the task has been aborted due to time-out (but not cancelled or aborted due to a Shutdown).</summary>
+        public abstract void Aborted();
+        /// <summary>Invoked whenever the state of this task changes.</summary>
+        /// <param name="old">The state of the task before this change.</param>
+        public virtual void StateChanged(TimeLimitedTaskState old) { }
         /// <summary>Gets the current state of this task.</summary>
-        public TimeLimitedTaskState State { get; internal set; }
+        public TimeLimitedTaskState State
+        {
+            get { return _state; }
+            internal set
+            {
+                if (_state == value) return;
+                var old = _state;
+                _state = value;
+                StateChanged(old);
+            }
+        }
+        private TimeLimitedTaskState _state = TimeLimitedTaskState.Created;
 
         /// <summary>Constructor.</summary>
-        /// <param name="timeLimit">See <see cref="TimeLimit"/>.</param>
-        /// <param name="action">See <see cref="TaskAction"/>.</param>
-        /// <param name="abortAction">See <see cref="AbortAction"/>.</param>
-        public TimeLimitedTask(TimeSpan timeLimit, Action action, Action abortAction = null)
+        /// <param name="timeLimit">The maximum amount of time this task may take from the point when it begins executing. Tasks exceeding this limit are automatically aborted.</param>
+        public TimeLimitedTask(TimeSpan timeLimit)
+        {
+            if (timeLimit < TimeSpan.Zero)
+                throw new ArgumentException("Time limit must not be negative", "timeLimit");
+            TimeLimit = timeLimit;
+        }
+    }
+
+    /// <summary>Implements an <see cref="Action"/>-based time-limited task for use with the <see cref="TimeLimitedThreadPool"/>.</summary>
+    public class TimeLimitedDelegateTask : TimeLimitedTask
+    {
+        private Action _action, _abortAction;
+
+        /// <summary>Constructor.</summary>
+        /// <param name="timeLimit">The maximum amount of time this task may take from the point when it begins executing. Tasks exceeding this limit are automatically aborted.</param>
+        /// <param name="action">The action invoked to perform the task in question. This code must be hardened against the asynchronous <see cref="ThreadAbortException"/>.</param>
+        /// <param name="abortAction">The action invoked if the task has been aborted due to time-out (but not cancelled or aborted due to a Shutdown).</param>
+        public TimeLimitedDelegateTask(TimeSpan timeLimit, Action action, Action abortAction = null)
+            : base(timeLimit)
         {
             if (action == null)
                 throw new ArgumentNullException("action");
-            if (timeLimit < TimeSpan.Zero)
-                throw new ArgumentException("Time limit must not be negative", "timeLimit");
-            TaskAction = action;
-            AbortAction = abortAction;
-            TimeLimit = timeLimit;
-            State = TimeLimitedTaskState.Created;
+            _action = action;
+            _abortAction = abortAction;
         }
+
+        /// <summary>Override; see base.</summary>
+        public override void Execute() { _action(); }
+        /// <summary>Override; see base.</summary>
+        public override void Aborted() { if (_abortAction != null) _abortAction(); }
     }
 }
