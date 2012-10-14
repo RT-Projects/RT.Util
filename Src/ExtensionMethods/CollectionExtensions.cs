0af5b7607ec287534d68a0640d382982eb3c80cb
Timwi <github@timwi.de>
2012-10-14 10:09:43 +0100

    Minor XML comment changes and a DRY fix (imported from Perforce changelist 3948)

--- Src/ExtensionMethods/CollectionExtensions.cs
+++ Src/ExtensionMethods/CollectionExtensions.cs
@@ -259,22 +259,20 @@ namespace RT.Util.ExtensionMethods
         /// Similar to <see cref="string.Substring(int)"/>, only for arrays. Returns a new array containing
         /// all items from the specified <paramref name="startIndex"/> onwards.
         /// </summary>
+        /// <remarks>Returns a new copy of the array even if <paramref name="startIndex"/> is 0.</remarks>
         public static T[] Subarray<T>(this T[] array, int startIndex)
         {
             if (array == null)
                 throw new ArgumentNullException("array");
-            if (startIndex < 0 || startIndex > array.Length)
-                throw new ArgumentOutOfRangeException("startIndex", "startIndex cannot be negative or extend beyond the end of the array.");
-            int length = array.Length - startIndex;
-            T[] result = new T[length];
-            Array.Copy(array, startIndex, result, 0, length);
-            return result;
+            return Subarray(array, startIndex, array.Length - startIndex);
         }
 
         /// <summary>
         /// Similar to <see cref="string.Substring(int,int)"/>, only for arrays. Returns a new array containing
         /// <paramref name="length"/> items from the specified <paramref name="startIndex"/> onwards.
         /// </summary>
+        /// <remarks>Returns a new copy of the array even if <paramref name="startIndex"/> is 0 and
+        /// <paramref name="length"/> is the length of the input array.</remarks>
         public static T[] Subarray<T>(this T[] array, int startIndex, int length)
         {
             if (array == null)
