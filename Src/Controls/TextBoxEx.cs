bc477451b06df1ccd828210136f877698de73ae4
Timwi <github@timwi.de>
2012-10-30 21:02:59 +0000

    - New control TextBoxEx: Same as TextBox, but selects all text when you press Ctrl+A. - Make TextBoxAutoHeight a TextBoxEx (imported from Perforce changelist 4017)

---
+++ Src/Controls/TextBoxEx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using RT.Util;
+using RT.Util.ExtensionMethods;
+using System.Windows.Forms;
+
+namespace RT.Util.Controls
+{
+    /// <summary>
+    ///     Provides a textbox with extra functionality (see remarks).</summary>
+    /// <remarks>
+    ///     <para>Extra functionality currently supported:</para>
+    ///     <list type="bullet">
+    ///         <item><description>Ctrl+A selects all text.</description></item>
+    ///     </list></remarks>
+    public class TextBoxEx : TextBox
+    {
+        /// <summary>Override; see base.</summary>
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            if (e.KeyData == (Keys.Control | Keys.A))
+            {
+                SelectAll();
+                e.Handled = true;
+                return;
+            }
+
+            base.OnKeyDown(e);
+        }
+    }
+}
