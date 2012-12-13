758b444b6410214464c317c7c6b29540a0e3e0a3
rstarkov <sourcecode@roman.st>
2012-12-13 12:27:03 +0000

    RunMain: made onUnhandledThread optional. (imported from Perforce changelist 4108)

--- Src/Ut/RunMain.cs
+++ Src/Ut/RunMain.cs
@@ -6,30 +6,38 @@ namespace RT.Util
     static partial class Ut
     {
         /// <summary>
-        /// Execute the core of the application. In debug mode, exceptions are passed through untouched. In release mode, all exceptions are caught,
-        /// including those on other threads, and passed on to the specified handlers (which must not be null). Also, the main thread is given the name "Main".
-        /// </summary>
-        /// <param name="main">A method which executes the core of the application.</param>
-        /// <param name="onUnhandledMain">Method called in case the main method throws an unhandled exception.</param>
-        /// <param name="onUnhandledThread">Method called in case a thread other than the one executing the main method throws an unhandled exception.</param>
-        public static void RunMain(Action main, Action<Exception> onUnhandledMain, Action<Exception> onUnhandledThread)
+        ///     Execute the core of the application. In debug mode, exceptions are passed through untouched. In release mode, all
+        ///     exceptions are caught, including those on other threads, and passed on to the specified handlers. Also, the main thread
+        ///     is given the name "Main".</summary>
+        /// <param name="main">
+        ///     A method which executes the core of the application.</param>
+        /// <param name="onUnhandledMain">
+        ///     Method called in case the main method throws an unhandled exception.</param>
+        /// <param name="onUnhandledThread">
+        ///     Method called in case a thread other than the one executing the main method throws an unhandled exception. If null,
+        ///     <paramref name="onUnhandledMain"/> will be called instead.</param>
+        public static void RunMain(Action main, Action<Exception> onUnhandledMain, Action<Exception> onUnhandledThread = null)
         {
             RunMain(() => { main(); return 0; }, excp => { onUnhandledMain(excp); return 0; }, onUnhandledThread);
         }
 
         /// <summary>
-        /// Execute the core of the application. In debug mode, exceptions are passed through untouched. In release mode, all exceptions are caught,
-        /// including those on other threads, and passed on to the specified handlers (which must not be null). Also, the main thread is given the name "Main".
-        /// </summary>
-        /// <param name="main">A method which executes the core of the application.</param>
-        /// <param name="onUnhandledMain">Method called in case the main method throws an unhandled exception. The return value is what this method will return.</param>
-        /// <param name="onUnhandledThread">Method called in case a thread other than the one executing the main method throws an unhandled exception.</param>
-        public static int RunMain(Func<int> main, Func<Exception, int> onUnhandledMain, Action<Exception> onUnhandledThread)
+        ///     Execute the core of the application. In debug mode, exceptions are passed through untouched. In release mode, all
+        ///     exceptions are caught, including those on other threads, and passed on to the specified handlers. Also, the main thread
+        ///     is given the name "Main".</summary>
+        /// <param name="main">
+        ///     A method which executes the core of the application.</param>
+        /// <param name="onUnhandledMain">
+        ///     Method called in case the main method throws an unhandled exception. The return value is what this method will return.</param>
+        /// <param name="onUnhandledThread">
+        ///     Method called in case a thread other than the one executing the main method throws an unhandled exception. If null,
+        ///     <paramref name="onUnhandledMain"/> will be called instead, and its return value will be ignored.</param>
+        public static int RunMain(Func<int> main, Func<Exception, int> onUnhandledMain, Action<Exception> onUnhandledThread = null)
         {
             if (onUnhandledMain == null)
                 throw new ArgumentNullException("onUnhandledMain");
             if (onUnhandledThread == null)
-                throw new ArgumentNullException("onUnhandledThread");
+                onUnhandledThread = excp => { onUnhandledMain(excp); };
 
             Thread.CurrentThread.Name = "Main";
 
