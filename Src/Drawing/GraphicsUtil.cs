036ebc6ac89be47b57a19567c2936effe6351516
Timwi <github@timwi.de>
2013-02-11 22:54:55 +0000

    GraphicsUtil: * Replace the (broken) color-from-HSL algorithm with a (working) from-HSV one * Fix the algorithm for generating alpha-transparent bitmaps (imported from Perforce changelist 4173)

--- Src/Drawing/GraphicsUtil.cs
+++ Src/Drawing/GraphicsUtil.cs
@@ -26,54 +26,101 @@ namespace RT.Util.Drawing
             return Color.FromArgb(r, g, b);
         }
 
-        /// <summary>Converts the specified hue, saturation and luminance values into an RGB color.</summary>
-        public static Color FromHsl(double hue, double saturation, double luminance)
+        public static Color FromHsv(double hue, double saturation, double value)
         {
-            double r = 0, g = 0, b = 0;
-            if (luminance != 0)
-            {
-                if (saturation == 0)
-                    r = g = b = luminance;
-                else
-                {
-                    double temp2 = getColorTemperature(hue, saturation, luminance);
-                    double temp1 = 2.0 * luminance - temp2;
+            if (hue < 0 || hue >= 360)
+                throw new ArgumentOutOfRangeException("hue", hue, "hue must be between 0 (incl.) and 360 (excl.).");
+            if (saturation < 0 || saturation > 1)
+                throw new ArgumentOutOfRangeException("saturation", saturation, "saturation must be between 0 and 1 (incl.).");
+            if (value < 0 || value > 1)
+                throw new ArgumentOutOfRangeException("value", value, "value must be between 0 and 1 (incl.).");
 
-                    r = getColorComponent(temp1, temp2, hue + 1.0 / 3.0);
-                    g = getColorComponent(temp1, temp2, hue);
-                    b = getColorComponent(temp1, temp2, hue - 1.0 / 3.0);
+            double h = hue;
+            double s = saturation;
+            double v = value;
+
+            double r = 0;
+            double g = 0;
+            double b = 0;
+
+            if (s == 0)
+            {
+                // If s is 0, all colors are the same.
+                // This is some flavor of gray.
+                r = v;
+                g = v;
+                b = v;
+            }
+            else
+            {
+                double p;
+                double q;
+                double t;
+
+                double fractionalSector;
+                int sectorNumber;
+                double sectorPos;
+
+                // The color wheel consists of 6 sectors.
+                // Figure out which sector you're in.
+                sectorPos = h / 60;
+                sectorNumber = (int) (Math.Floor(sectorPos));
+
+                // get the fractional part of the sector.
+                // That is, how many degrees into the sector
+                // are you?
+                fractionalSector = sectorPos - sectorNumber;
+
+                // Calculate values for the three axes
+                // of the color.
+                p = v * (1 - s);
+                q = v * (1 - (s * fractionalSector));
+                t = v * (1 - (s * (1 - fractionalSector)));
+
+                // Assign the fractional colors to r, g, and b
+                // based on the sector the angle is in.
+                switch (sectorNumber)
+                {
+                    case 0:
+                        r = v;
+                        g = t;
+                        b = p;
+                        break;
+
+                    case 1:
+                        r = q;
+                        g = v;
+                        b = p;
+                        break;
+
+                    case 2:
+                        r = p;
+                        g = v;
+                        b = t;
+                        break;
+
+                    case 3:
+                        r = p;
+                        g = q;
+                        b = v;
+                        break;
+
+                    case 4:
+                        r = t;
+                        g = p;
+                        b = v;
+                        break;
+
+                    case 5:
+                        r = v;
+                        g = p;
+                        b = q;
+                        break;
                 }
             }
-            return Color.FromArgb((int) (255 * r), (int) (255 * g), (int) (255 * b));
-        }
-
-        private static double getColorComponent(double temp1, double temp2, double temp3)
-        {
-            temp3 = moveIntoRange(temp3);
-            if (temp3 < 1.0 / 6.0)
-                return temp1 + (temp2 - temp1) * 6.0 * temp3;
-            else if (temp3 < 0.5)
-                return temp2;
-            else if (temp3 < 2.0 / 3.0)
-                return temp1 + ((temp2 - temp1) * ((2.0 / 3.0) - temp3) * 6.0);
-            else
-                return temp1;
-        }
-
-        private static double moveIntoRange(double temp3)
-        {
-            if (temp3 < 0.0)
-                temp3 += 1.0;
-            else if (temp3 > 1.0)
-                temp3 -= 1.0;
-            return temp3;
-        }
-
-        private static double getColorTemperature(double hue, double saturation, double luminance)
-        {
-            return luminance < 0.5
-                ? luminance * (1.0 + saturation)
-                : luminance + saturation - (luminance * saturation);
+            // return an RgbColor structure, with values scaled
+            // to be between 0 and 255.
+            return Color.FromArgb((int) (r * 255), (int) (g * 255), (int) (b * 255));
         }
 
         /// <summary>
@@ -172,7 +219,7 @@ namespace RT.Util.Drawing
                 byte* read = (byte*) tBits.Scan0 + y * tBits.Stride;
                 byte* write = (byte*) oBits.Scan0 + y * oBits.Stride;
                 for (int x = 0; x < width; x++)
-                    write[4 * x + 3] = Math.Min(write[4 * x + 3], read[4 * x + 3]);
+                    write[4 * x + 3] = Math.Min(write[4 * x + 3], read[4 * x]);
             }
             opaque.UnlockBits(oBits);
             trans.UnlockBits(tBits);
