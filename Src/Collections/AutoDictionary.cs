d5a6d9b39045df1d779b6b0f49d540a4eb6df2e7
rstarkov <sourcecode@roman.st>
2013-04-02 16:45:56 +0100

    Improved AutoList. Added AutoDictionary. (imported from Perforce changelist 4387)

---
+++ Src/Collections/AutoDictionary.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RT.Util.Collections
+{
+    /// <summary>
+    ///     Implements a dictionary with a slightly different indexer, namely one that pretends every key has been pre-initialized
+    ///     using an initializer function. See Remarks.</summary>
+    /// <remarks>
+    ///     Only the indexer behaviour is changed; in every other way this behaves just like a standard, non-prepopulated
+    ///     dictionary. Moreover, the implementation is such that the new behaviour is only effective when used directly through
+    ///     the class; accessing the indexer through the <c>IDictionary</c> interface or the <c>Dictionary</c> base class will
+    ///     currently behave the same as it would for a standard dictionary.</remarks>
+    public class AutoDictionary<TKey, TValue> : Dictionary<TKey, TValue>
+    {
+        private Func<TKey, TValue> _initializer;
+
+        /// <summary>
+        ///     Gets or sets the element with the specified key. When getting a key that hasn't been set before, the getter
+        ///     behaves as if the dictionary has been pre-populated for every possible key using the initializer function (or
+        ///     default(TValue) if not provided).</summary>
+        public new TValue this[TKey key]
+        {
+            get
+            {
+                TValue v;
+                if (TryGetValue(key, out v))
+                    return v;
+                v = _initializer == null ? default(TValue) : _initializer(key);
+                Add(key, v);
+                return v;
+            }
+
+            set
+            {
+                base[key] = value;
+            }
+        }
+
+        /// <summary>Constructor.</summary>
+        public AutoDictionary(Func<TKey, TValue> initializer = null)
+            : base()
+        {
+            _initializer = initializer;
+        }
+        /// <summary>Constructor.</summary>
+        public AutoDictionary(int capacity, Func<TKey, TValue> initializer = null)
+            : base(capacity)
+        {
+            _initializer = initializer;
+        }
+        /// <summary>Constructor.</summary>
+        public AutoDictionary(IEqualityComparer<TKey> comparer, Func<TKey, TValue> initializer = null)
+            : base(comparer)
+        {
+            _initializer = initializer;
+        }
+        /// <summary>Constructor.</summary>
+        public AutoDictionary(IDictionary<TKey, TValue> dictionary, Func<TKey, TValue> initializer = null)
+            : base(dictionary)
+        {
+            _initializer = initializer;
+        }
+        /// <summary>Constructor.</summary>
+        public AutoDictionary(int capacity, IEqualityComparer<TKey> comparer, Func<TKey, TValue> initializer = null)
+            : base(capacity, comparer)
+        {
+            _initializer = initializer;
+        }
+        /// <summary>Constructor.</summary>
+        public AutoDictionary(IDictionary<TKey, TValue> dictionary, IEqualityComparer<TKey> comparer, Func<TKey, TValue> initializer = null)
+            : base(dictionary, comparer)
+        {
+            _initializer = initializer;
+        }
+    }
+}
--- Src/Collections/AutoList.cs
+++ Src/Collections/AutoList.cs
@@ -1,35 +1,86 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace RT.Util.Collections
 {
-    /// <summary>Encapsulates a list which dynamically grows as items are written to non-existent indexes. The indexes are filled with the type argument’s default value.</summary>
+    /// <summary>
+    ///     Encapsulates a list which dynamically grows as items are written to non-existent indexes. Any gaps are populated with
+    ///     default values. The behaviour of this list's indexed getter and setter is indistinguishable from that of an infinitely
+    ///     long list pre-populated by invoking the initializer function (assuming it is side-effect free). See Remarks.</summary>
+    /// <remarks>
+    ///     <para>
+    ///         Only the indexer behaviour is changed; in every other way this behaves just like a standard, non-infinite list.
+    ///         Moreover, the implementation is such that the new behaviour is only effective when used directly through the
+    ///         class; accessing the indexer through the <c>IList</c> interface or the <c>List</c> base class will currently
+    ///         behave the same as it would for a standard list.</para>
+    ///     <para>
+    ///         Note that this is not a sparse list; accessing elements at a given index will grow the list to contain all of the
+    ///         items below the index too.</para></remarks>
     public class AutoList<T> : List<T>
     {
-        /// <summary>Gets or sets the element at the specified index.</summary>
-        /// <param name="index">The zero-based index of the element to get or set.</param>
-        /// <returns>The element at the specified index, or the type argument’s default value if the index does not exist.</returns>
-        /// <exception cref="System.ArgumentOutOfRangeException">index is less than 0.</exception>
+        private Func<int, T> _initializer;
+
+        /// <summary>
+        ///     Gets or sets the element at the specified index. The behaviour of both the getter and the setter is
+        ///     indistinguishable from that of an infinitely long list pre-populated by invoking the initializer function
+        ///     (assuming it is side-effect free).</summary>
         public new T this[int index]
         {
             get
             {
-                return index >= Count ? default(T) : base[index];
+                if (_initializer == null)
+                    return index >= Count ? default(T) : base[index];
+                // default(T) cannot possibly create a value that we'd need to store immediately in order to preserve the infinite list illusion,
+                // so do not grow the list for this if the user supplied no initializer.
+
+                while (index >= Count)
+                    Add(_initializer(Count));
+                return base[index];
             }
+
             set
             {
                 while (index >= Count)
-                    Add(default(T));
+                    Add(_initializer == null ? default(T) : _initializer(Count));
                 base[index] = value;
             }
         }
 
-        /// <summary>Constructor.</summary>
-        public AutoList() : base() { }
-        /// <summary>Constructor.</summary>
-        /// <param name="capacity">The number of elements that the new list can initially store.</param>
-        public AutoList(int capacity) : base(capacity) { }
-        /// <summary>Constructor.</summary>
-        /// <param name="collection">A collection whose elements are copied to the new list.</param>
-        public AutoList(IEnumerable<T> collection) : base(collection) { }
+        /// <summary>
+        ///     Constructor.</summary>
+        /// <param name="initializer">
+        ///     A function which creates a value to be used for non-existent elements upon their creation. If <c>null</c>,
+        ///     <c>default(T)</c> is used instead.</param>
+        public AutoList(Func<int, T> initializer = null)
+            : base()
+        {
+            _initializer = initializer;
+        }
+
+        /// <summary>
+        ///     Constructor.</summary>
+        /// <param name="capacity">
+        ///     The number of elements that the new list can initially store.</param>
+        /// <param name="initializer">
+        ///     A function which creates a value to be used for non-existent elements upon their creation. If <c>null</c>,
+        ///     <c>default(T)</c> is used instead.</param>
+        public AutoList(int capacity, Func<int, T> initializer = null)
+            : base(capacity)
+        {
+            _initializer = initializer;
+        }
+
+        /// <summary>
+        ///     Constructor.</summary>
+        /// <param name="collection">
+        ///     A collection whose elements are copied to the new list.</param>
+        /// <param name="initializer">
+        ///     A function which creates a value to be used for non-existent elements upon their creation. If <c>null</c>,
+        ///     <c>default(T)</c> is used instead.</param>
+        public AutoList(IEnumerable<T> collection, Func<int, T> initializer = null)
+            : base(collection)
+        {
+            _initializer = initializer;
+        }
     }
 }
