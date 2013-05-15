300d913429abf360fd9a36839a5d94863d7679f3
Timwi <github@timwi.de>
2013-05-15 15:40:35 +0100

    .LastIndexOf() extension method (imported from Perforce changelist 4485)

--- Src/ExtensionMethods/IEnumerableExtensions.cs
+++ Src/ExtensionMethods/IEnumerableExtensions.cs
@@ -427,6 +427,39 @@ namespace RT.Util.ExtensionMethods
         }
 
         /// <summary>
+        /// Returns the index of the last element in this <paramref name="source"/> satisfying
+        /// the specified <paramref name="predicate"/>. If no such elements are found, returns <c>-1</c>.
+        /// </summary>
+        /// <remarks>This method is optimised for the case in which the input sequence is a list or array.
+        /// In all other cases, the collection is evaluated completely before this method returns.</remarks>
+        public static int LastIndexOf<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+            if (predicate == null)
+                throw new ArgumentNullException("predicate");
+
+            var list = source as IList<T>;
+            if (list != null)
+            {
+                var i = list.Count - 1;
+                while (i >= 0 && !predicate(list[i]))
+                    i--;
+                return i;
+            }
+
+            int index = 0;
+            int lastIndex = -1;
+            foreach (var v in source)
+            {
+                if (predicate(v))
+                    lastIndex = index;
+                index++;
+            }
+            return lastIndex;
+        }
+
+        /// <summary>
         /// Returns the index of the first element in this <paramref name="source"/> that is equal to
         /// the specified <paramref name="element"/> as determined by the specified <paramref name="comparer"/>.
         /// If no such elements are found, returns <c>-1</c>.
