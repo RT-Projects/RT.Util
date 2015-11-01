33ce2c756f7e834bf1d1fd3319393f9b815f0d68
rstarkov <sourcecode@roman.st>
2015-11-01 20:08:54 +0000

    DlgMessage: reformatted comments. (imported from Perforce changelist 5810)

--- Src/Forms/DlgMessage.cs
+++ Src/Forms/DlgMessage.cs
@@ -7,9 +7,8 @@ using RT.Util.ExtensionMethods;
 namespace RT.Util.Dialogs
 {
     /// <summary>
-    /// Selects the type of dialog for <see cref="DlgMessage"/> to show. This specifies the sound and, unless
-    /// explicitly overridden, the image and caption used to show the dialog.
-    /// </summary>
+    ///     Selects the type of dialog for <see cref="DlgMessage"/> to show. This specifies the sound and, unless explicitly
+    ///     overridden, the image and caption used to show the dialog.</summary>
     public enum DlgType
     {
         /// <summary>Displays an information dialog.</summary>
@@ -21,32 +20,22 @@ namespace RT.Util.Dialogs
         /// <summary>Displays an error dialog.</summary>
         Error,
         /// <summary>
-        /// Displays a custom dialog. Custom defaults to no sound, no default image,
-        /// and the caption defaults to the application name (all are overridable).
-        /// Doesn't fully work, bug 15.
-        /// </summary>
+        ///     Displays a custom dialog. Custom defaults to no sound, no default image, and the caption defaults to the
+        ///     application name (all are overridable). Doesn't fully work, bug 15.</summary>
         Custom
     }
 
-    /// <summary>
-    /// Specifies whether the text in a <see cref="DlgMessage"/> box should be rendered using EggsML or plain text.
-    /// </summary>
+    /// <summary>Specifies whether the text in a <see cref="DlgMessage"/> box should be rendered using EggsML or plain text.</summary>
     public enum DlgMessageFormat
     {
-        /// <summary>
-        /// Indicates plain text without formatting.
-        /// </summary>
+        /// <summary>Indicates plain text without formatting.</summary>
         PlainText,
-        /// <summary>
-        /// Indicates EggsML mark-up.
-        /// </summary>
+        /// <summary>Indicates EggsML mark-up.</summary>
         EggsML,
     }
 
     /// <summary>
-    /// This class is used by the <see cref="DlgMessage"/> class. This is the form used
-    /// for displaying the message box.
-    /// </summary>
+    ///     This class is used by the <see cref="DlgMessage"/> class. This is the form used for displaying the message box.</summary>
     internal sealed partial class DlgMessageForm : Form
     {
         private sealed class DlgMessageFormResources
@@ -99,17 +88,13 @@ namespace RT.Util.Dialogs
             }
         }
 
-        /// <summary>
-        /// Change this variable to i18n'ize the default captions
-        /// </summary>
+        /// <summary>Change this variable to i18n'ize the default captions</summary>
         internal static string[] DefaultCaption = new string[] {
             "Information", "Question",
             "Warning", "Error", "" // the last string is for "custom"
         };
 
-        /// <summary>
-        /// Change this variable to i18n'ize the default images
-        /// </summary>
+        /// <summary>Change this variable to i18n'ize the default images</summary>
         internal static Bitmap[] DefaultImage = new Bitmap[] { null, null, null, null, null };
 
         internal static Bitmap GetDefaultImage(DlgType type)
@@ -127,9 +112,7 @@ namespace RT.Util.Dialogs
             return null;
         }
 
-        /// <summary>
-        /// Change this variable to i18n'ize the default OK button text
-        /// </summary>
+        /// <summary>Change this variable to i18n'ize the default OK button text</summary>
         internal static string DefaultOKCaption = "&OK";
 
         /// <summary>Creates an instance of the DlgMessage form.</summary>
@@ -139,9 +122,8 @@ namespace RT.Util.Dialogs
         }
 
         /// <summary>
-        /// This takes care of the user closing the dialog. This is equivalent to pressing
-        /// the Cancel button, whichever one it happens to be.
-        /// </summary>
+        ///     This takes care of the user closing the dialog. This is equivalent to pressing the Cancel button, whichever
+        ///     one it happens to be.</summary>
         private void DlgMessage_FormClosing(object sender, FormClosingEventArgs e)
         {
             if (DialogResult == DialogResult.None)
@@ -150,127 +132,98 @@ namespace RT.Util.Dialogs
     }
 
     /// <summary>
-    /// Holds a number of settings that define a message dialog. Provides a method to show the
-    /// dialog represented, as well as static methods to show some common dialog kinds with
-    /// most settings at their defaults.
-    /// </summary>
+    ///     Holds a number of settings that define a message dialog. Provides a method to show the dialog represented, as well
+    ///     as static methods to show some common dialog kinds with most settings at their defaults.</summary>
     public sealed class DlgMessage
     {
         /// <summary>
-        /// Specifies the message type. This selects a sound, an image and a caption to be used
-        /// by default, unless explicitly overridden in the other fields of this class. Defaults
-        /// to <see cref="DlgType.Info"/>.
-        /// </summary>
+        ///     Specifies the message type. This selects a sound, an image and a caption to be used by default, unless
+        ///     explicitly overridden in the other fields of this class. Defaults to <see cref="DlgType.Info"/>.</summary>
         public DlgType Type = DlgType.Info;
         /// <summary>
-        /// Specifies the labels of the buttons to be displayed in the dialog. Defaults to null,
-        /// in which case a single "OK" button will appear. May contain at most four elements.
-        /// The labels may include the ampersand to indicate a shortcut key, or double-ampersand
-        /// to include an ampersand.
-        /// </summary>
+        ///     Specifies the labels of the buttons to be displayed in the dialog. Defaults to null, in which case a single
+        ///     "OK" button will appear. May contain at most four elements. The labels may include the ampersand to indicate a
+        ///     shortcut key, or double-ampersand to include an ampersand.</summary>
         public string[] Buttons;
         /// <summary>
-        /// Specifies the message to be displayed in the main message area. Defaults to null,
-        /// in which case the message area will be empty.
-        /// </summary>
+        ///     Specifies the message to be displayed in the main message area. Defaults to null, in which case the message
+        ///     area will be empty.</summary>
         public string Message;
-        /// <summary>
-        /// Specifies whether the text in this message box should be rendered using EggsML or plain text.
-        /// </summary>
+        /// <summary>Specifies whether the text in this message box should be rendered using EggsML or plain text.</summary>
         public DlgMessageFormat Format = DlgMessageFormat.PlainText;
         /// <summary>
-        /// Specifies a caption to be displayed in the dialog's window title bar. Defaults to null,
-        /// which means choose a title based on the <see cref="Type"/> field.
-        /// </summary>
+        ///     Specifies a caption to be displayed in the dialog's window title bar. Defaults to null, which means choose a
+        ///     title based on the <see cref="Type"/> field.</summary>
         public string Caption;
         /// <summary>
-        /// Specifies an image to be displayed in the image area of the dialog. Defaults to null,
-        /// which means use an image based on the <see cref="Type"/> field. If the type is <see cref="DlgType.Custom"/>,
-        /// and this field is null, no image will be displayed.
-        /// </summary>
+        ///     Specifies an image to be displayed in the image area of the dialog. Defaults to null, which means use an image
+        ///     based on the <see cref="Type"/> field. If the type is <see cref="DlgType.Custom"/>, and this field is null, no
+        ///     image will be displayed.</summary>
         public Bitmap Image;    // null acceptable as final value
         /// <summary>
-        /// Specifies a font to be used for the message text (but not the buttons). Defaults to
-        /// null, which means the default system font should be used.
-        /// </summary>
+        ///     Specifies a font to be used for the message text (but not the buttons). Defaults to null, which means the
+        ///     default system font should be used.</summary>
         public Font Font;       // null acceptable as final value
         /// <summary>
-        /// Specifies the index of the accept button (the one selected if the user just presses Enter).
-        /// Defaults to -1, which means the button with index 0.
-        /// </summary>
+        ///     Specifies the index of the accept button (the one selected if the user just presses Enter). Defaults to -1,
+        ///     which means the button with index 0.</summary>
         public int AcceptButton = -1;
         /// <summary>
-        /// Specifies the index of the cancel button (the one selected if the user just presses Esc or Alt+F4).
-        /// Defaults to -1, which means the button with the largest valid index.
-        /// </summary>
+        ///     Specifies the index of the cancel button (the one selected if the user just presses Esc or Alt+F4). Defaults
+        ///     to -1, which means the button with the largest valid index.</summary>
         public int CancelButton = -1;
 
         /// <summary>Specifies whether a taskbar icon is shown for the dialog box.</summary>
         public bool ShowInTaskbar;
 
         /// <summary>
-        /// Shows a message using all the settings specified in this class instance. Anything
-        /// left at defaults will be modified to hold the appropriate value. Any invalid settings
-        /// will be flagged with an exception.
-        /// </summary>
+        ///     Shows a message using all the settings specified in this class instance. Anything left at defaults will be
+        ///     modified to hold the appropriate value. Any invalid settings will be flagged with an exception.</summary>
         public int Show()
         {
             deduceAndCheckSettings();
             return showAsIs();
         }
 
-        /// <summary>
-        /// Shows a message of the specified message type, using an appropriate caption, sound and image.
-        /// </summary>
+        /// <summary>Shows a message of the specified message type, using an appropriate caption, sound and image.</summary>
         public static int Show(string message, DlgType type)
         {
             return new DlgMessage() { Message = message, Type = type }.Show();
         }
 
-        /// <summary>
-        /// Shows a message of the specified message type, using an appropriate sound and image.
-        /// </summary>
+        /// <summary>Shows a message of the specified message type, using an appropriate sound and image.</summary>
         public static int Show(string message, string caption, DlgType type, params string[] buttons)
         {
             return new DlgMessage() { Message = message, Caption = caption, Type = type, Buttons = buttons }.Show();
         }
 
-        /// <summary>
-        /// Shows an informational message using the image, caption and sound appropriate for this message type.
-        /// </summary>
+        /// <summary>Shows an informational message using the image, caption and sound appropriate for this message type.</summary>
         public static int ShowInfo(string message, params string[] buttons)
         {
             return new DlgMessage() { Message = message, Buttons = buttons, Type = DlgType.Info }.Show();
         }
 
-        /// <summary>
-        /// Shows a question message using the image, caption and sound appropriate for this message type.
-        /// </summary>
+        /// <summary>Shows a question message using the image, caption and sound appropriate for this message type.</summary>
         public static int ShowQuestion(string message, params string[] buttons)
         {
             return new DlgMessage() { Message = message, Buttons = buttons, Type = DlgType.Question }.Show();
         }
 
-        /// <summary>
-        /// Shows a warning message using the image, caption and sound appropriate for this message type.
-        /// </summary>
+        /// <summary>Shows a warning message using the image, caption and sound appropriate for this message type.</summary>
         public static int ShowWarning(string message, params string[] buttons)
         {
             return new DlgMessage() { Message = message, Buttons = buttons, Type = DlgType.Warning }.Show();
         }
 
-        /// <summary>
-        /// Shows an error message using the image, caption and sound appropriate for this message type.
-        /// </summary>
+        /// <summary>Shows an error message using the image, caption and sound appropriate for this message type.</summary>
         public static int ShowError(string message, params string[] buttons)
         {
             return new DlgMessage() { Message = message, Buttons = buttons, Type = DlgType.Error }.Show();
         }
 
         /// <summary>
-        /// Verifies that all the settings are correct, throwing an exception if not. For all
-        /// settings left at defaults, stores a value deemed appropriate based on the other settings.
-        /// </summary>
+        ///     Verifies that all the settings are correct, throwing an exception if not. For all settings left at defaults,
+        ///     stores a value deemed appropriate based on the other settings.</summary>
         private void deduceAndCheckSettings()
         {
             if (Buttons == null || Buttons.Length == 0)
@@ -298,11 +251,10 @@ namespace RT.Util.Dialogs
         }
 
         /// <summary>
-        /// Creates a message form and fills in the controls using the current settings. Does not
-        /// verify whether the settings are valid, so must only be used internally and only after the
-        /// settings have been verified.
-        /// </summary>
-        /// <returns>The index of the button pressed.</returns>
+        ///     Creates a message form and fills in the controls using the current settings. Does not verify whether the
+        ///     settings are valid, so must only be used internally and only after the settings have been verified.</summary>
+        /// <returns>
+        ///     The index of the button pressed.</returns>
         private int showAsIs()
         {
             using (var form = new DlgMessageForm())
@@ -390,12 +342,19 @@ namespace RT.Util.Dialogs
             }
         }
 
-        /// <summary>Applies a translation to certain default strings, on the assumption that all messages are to be shown with the same translation.</summary>
-        /// <param name="ok">Translation for the default "OK" button.</param>
-        /// <param name="captionInfo">Translation for the default window caption for informational messages.</param>
-        /// <param name="captionQuestion">Translation for the default window caption for question messages.</param>
-        /// <param name="captionWarning">Translation for the default window caption for warning messages.</param>
-        /// <param name="captionError">Translation for the default window caption for error messages.</param>
+        /// <summary>
+        ///     Applies a translation to certain default strings, on the assumption that all messages are to be shown with the
+        ///     same translation.</summary>
+        /// <param name="ok">
+        ///     Translation for the default "OK" button.</param>
+        /// <param name="captionInfo">
+        ///     Translation for the default window caption for informational messages.</param>
+        /// <param name="captionQuestion">
+        ///     Translation for the default window caption for question messages.</param>
+        /// <param name="captionWarning">
+        ///     Translation for the default window caption for warning messages.</param>
+        /// <param name="captionError">
+        ///     Translation for the default window caption for error messages.</param>
         public static void Translate(string ok, string captionInfo, string captionQuestion, string captionWarning, string captionError)
         {
             DlgMessageForm.DefaultOKCaption = ok;
