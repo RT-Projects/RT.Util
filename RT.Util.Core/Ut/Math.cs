8b2eaad9cd37eee1cef41be9f6d2d585d6f53029
Timwi <github@timwi.de>
2022-05-31 20:29:00 +0200

    Ut.Gcd()

---
+++ RT.Util.Core/Ut/Math.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RT.Util
+{
+#if EXPORT_UTIL
+    public
+#endif
+    static partial class Ut
+    {
+        /// <summary>Calculates the greatest common divisor of <paramref name="a"/> and <paramref name="b"/>.</summary>
+        public static int Gcd(int a, int b)
+        {
+            while (a != 0 && b != 0)
+            {
+                if (a > b)
+                    a %= b;
+                else
+                    b %= a;
+            }
+            return a | b;
+        }
+        /// <summary>Calculates the greatest common divisor of <paramref name="a"/> and <paramref name="b"/>.</summary>
+        public static uint Gcd(uint a, uint b)
+        {
+            while (a != 0 && b != 0)
+            {
+                if (a > b)
+                    a %= b;
+                else
+                    b %= a;
+            }
+            return a | b;
+        }
+        /// <summary>Calculates the greatest common divisor of <paramref name="a"/> and <paramref name="b"/>.</summary>
+        public static long Gcd(long a, long b)
+        {
+            while (a != 0 && b != 0)
+            {
+                if (a > b)
+                    a %= b;
+                else
+                    b %= a;
+            }
+            return a | b;
+        }
+        /// <summary>Calculates the greatest common divisor of <paramref name="a"/> and <paramref name="b"/>.</summary>
+        public static ulong Gcd(ulong a, ulong b)
+        {
+            while (a != 0 && b != 0)
+            {
+                if (a > b)
+                    a %= b;
+                else
+                    b %= a;
+            }
+            return a | b;
+        }
+    }
+}
