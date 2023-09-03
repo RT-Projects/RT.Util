308d8b25d29a949c536cf66714cfecc1132277ef
Roman Starkov <sourcecode@roman.st>
2023-09-03 22:12:42 +0100

    CommandRunner: cleaner tests and trickier cases

--- Tests/RT.Util.Core.Tests/CommandRunnerTests.cs
+++ Tests/RT.Util.Core.Tests/CommandRunnerTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
@@ -34,6 +35,12 @@ namespace RT.Util
             testArgs(@"test >nul");
             testArgs(@"test stuff", ">nul");
             testArgs(@"test stuff \""&whoami");
+            testArgs(@"%PATH%");
+            testArgs("\"%PATH%\"");
+            testArgs("^%PATH^%");
+            testArgs("""
+                --folder="C:\Program Files\"
+                """);
         }
 
         private static void testArgs(params string[] args)
@@ -43,38 +50,50 @@ namespace RT.Util
 
         private static void testArgs(bool checkTrivial, params string[] args)
         {
-#if false
+            if (checkTrivial)
+                Assert.AreEqual(args.JoinString(" "), CommandRunner.ArgsToCommandLine(args));
+            var path1 = @"C:\Temp\PrintArgs\PrintArgs.exe";
+            if (File.Exists(path1))
+                testArgsReal(new[] { path1 }.Concat(args).ToArray());
+            else
+                testArgsEmulated(args);
+            var path2 = @"C:\Temp\PrintArgs\With Space\PrintArgs.exe";
+            if (File.Exists(path2))
+                testArgsReal(new[] { path2 }.Concat(args).ToArray());
+        }
+
+        private static void testArgsReal(string[] args)
+        {
             // This requires "printargs", which is an external binary that uses CommandLineToArgvW(GetCommandLine()) to parse and print the arguments it has received.
             // This is a true test of what really happens. Without printargs, this test has to emulate a fair chunk of what happens behind the scenes.
             var cmd = new CommandRunner();
-            cmd.SetCommand(new[] { "printargs" }.Concat(args));
-            if (checkTrivial)
-                Assert.AreEqual("printargs " + args.JoinString(" "), cmd.Command);
+            cmd.SetCommand(args);
             cmd.CaptureEntireStdout = true;
             cmd.StartAndWait();
             var lines = cmd.EntireStdout.FromUtf8().Split("\n").Select(l => l.Trim()).Where(l => l != "").ToArray();
             Assert.AreEqual(args.Length, lines.Length - 2);
-            var countS = Regex.Match(lines[0], @"^Count: \[(\d+)\]$");
-            Assert.IsTrue(countS.Success);
-            Assert.IsTrue(lines.Last() == "END");
-            var count = int.Parse(countS.Groups[1].Value);
-            Assert.AreEqual(count, args.Length);
+            Assert.AreEqual($"Count: [{args.Length}]", lines[0]);
+            Assert.AreEqual("END", lines.Last());
             for (int i = 0; i < args.Length; i++)
-            {
-                var matchedArgS = Regex.Match(lines[i + 1], @"^{0}: \[(.*?)\]$".Fmt(i + 1));
-                Assert.IsTrue(matchedArgS.Success);
-                Assert.AreEqual(args[i], matchedArgS.Groups[1].Value);
-            }
-#else
+                Assert.AreEqual($"{i}: [{args[i]}]", lines[i + 1]);
+        }
+
+        private static void testArgsEmulated(string[] args)
+        {
             // Partially emulate what happens to the command and check that the received arguments are identical to the arguments passed in.
             // This obviously doesn't test everything, in particular it completely skips the processing done by cmd.exe
-            var str = CommandRunner.ArgsToCommandLine(args);
-            if (checkTrivial)
-                Assert.AreEqual(args.JoinString(" "), str);
-            var argsSplit = NativeSplit(str);
-            Assert.IsTrue(args.SequenceEqual(argsSplit));
-#endif
+            var args1 = "C:\\dummypath.exe".Concat(args).ToArray();
+            var str1 = CommandRunner.ArgsToCommandLine(args1);
+            var argsSplit1 = NativeSplit(str1);
+            Assert.IsTrue(args1.SequenceEqual(argsSplit1));
+            var args2 = "C:\\Program Files\\dummypath.exe".Concat(args).ToArray();
+            var str2 = CommandRunner.ArgsToCommandLine(args2);
+            var argsSplit2 = NativeSplit(str2);
+            Assert.IsTrue(args2.SequenceEqual(argsSplit2));
+        }
 
+        private static void testArgsEcho(string[] args)
+        {
             // "echo" is a special case: it does not parse its arguments using CommandLineToArgvW; it's just a cmd.exe command that outputs the arguments "as-is".
             // Test that we do in fact get them back as-is.
             var naivelyJoinedArgs = args.JoinString(" ");
@@ -89,13 +108,13 @@ namespace RT.Util
             IntPtr result = IntPtr.Zero;
             try
             {
-                result = CommandLineToArgvW(@"C:\dummypath.exe " + commandline, out count); // because this function acts inconsistently when parsing an empty string
+                result = CommandLineToArgvW(commandline, out count);
                 if (result == IntPtr.Zero)
                     throw new Exception("420s7");
 
-                var args = new string[count - 1];
-                for (int i = 1; i < count; i++)
-                    args[i - 1] = Marshal.PtrToStringUni(Marshal.ReadIntPtr(result, i * IntPtr.Size));
+                var args = new string[count];
+                for (int i = 0; i < count; i++)
+                    args[i] = Marshal.PtrToStringUni(Marshal.ReadIntPtr(result, i * IntPtr.Size));
                 return args;
             }
             finally
