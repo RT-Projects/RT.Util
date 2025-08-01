c4ad53d8bf89cfad264e7f64263bce9357363666
Timwi <github@timwi.de>
2025-08-01 14:30:05 +0200

    Voronoi test

--- Tests/RT.Geometry.Tests/VoronoiDiagramTests.cs
+++ Tests/RT.Geometry.Tests/VoronoiDiagramTests.cs
@@ -45,4 +45,17 @@ public sealed class VoronoiDiagramTests
         var vd = VoronoiDiagram.GenerateVoronoiDiagram(sites, 14, 15, VoronoiDiagramFlags.IncludeEdgePolygons);
         Assert.AreEqual(4, vd.Polygons.Length);
     }
+
+    [Test]
+    public static void TestVerticesThatAreNearlyVertical()
+    {
+        var points = new[]
+        {
+            new PointD(0.41586468492672696, 45.06283759329131),
+            new PointD(0.1100000000001, 45.089104420403665),
+            new PointD(0.11, 44.1)
+        };
+        var voronoi = VoronoiDiagram.GenerateVoronoiDiagram(points, 1, 47, VoronoiDiagramFlags.IncludeEdgePolygons);
+        Assert.IsTrue(voronoi.Polygons.All(poly => poly.Vertices.All(v => !double.IsNaN(v.X) && !double.IsNaN(v.Y))));
+    }
 }
